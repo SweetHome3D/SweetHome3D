d774be78c1546ae789b371e6c1060e150072baf5
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2005-11-09 14:09:39 +0000

    Updated javadoc Changed printed text
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@4 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- test/com/eteks/sweethome3d/test/EclipseTest.java
+++ test/com/eteks/sweethome3d/test/EclipseTest.java
@@ -2,10 +2,10 @@ package com.eteks.sweethome3d.test;
 
 public class EclipseTest {
   /**
-   * @param args
+   * Test for Eclipse IDE.
+   * @param args no argument required.
    */
   public static void main(String [] args) {
-    // TODO Auto-generated method stub
-    System.out.println("Hello World");
+    System.out.println("Hello future Sweet Home 3D user");
   }
 }
