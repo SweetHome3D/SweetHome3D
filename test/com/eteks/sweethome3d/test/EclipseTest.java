c515e5a3b03b802231b54a597d58f685c488cf7a
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2005-11-09 08:11:04 +0000

    Project start
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@3 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

---
+++ test/com/eteks/sweethome3d/test/EclipseTest.java
@@ -0,0 +1,11 @@
+package com.eteks.sweethome3d.test;
+
+public class EclipseTest {
+  /**
+   * @param args
+   */
+  public static void main(String [] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello World");
+  }
+}
