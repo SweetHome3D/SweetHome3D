69bf66e9632f3e8a98fc44c6327adea9f26d430f
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2006-04-29 04:52:22 +0000

    Implemented furniture tree
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@39 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/swing/CatalogTree.java
+++ src/com/eteks/sweethome3d/swing/CatalogTree.java
@@ -19,9 +19,25 @@
  */
 package com.eteks.sweethome3d.swing;
 
+import java.awt.Component;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
 
 import com.eteks.sweethome3d.model.Catalog;
+import com.eteks.sweethome3d.model.Category;
 import com.eteks.sweethome3d.model.PieceOfFurniture;
 
 /**
@@ -30,11 +46,80 @@ import com.eteks.sweethome3d.model.PieceOfFurniture;
  */
 public class CatalogTree extends JTree {
   public CatalogTree(Catalog catalog) {
-    // TODO Auto-generated constructor stub
+    // Create Nodes hierarchy from DefaultFurniture 
+    DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+    for(Category category : catalog.getCategories()) {
+      DefaultMutableTreeNode categoryNode = new DefaultMutableTreeNode (category);
+      root.add(categoryNode);
+      for (PieceOfFurniture piece : category.getFurniture()) {
+        categoryNode.add(new DefaultMutableTreeNode (piece, false));
+      }      
+    }
+      
+    setModel(new DefaultTreeModel (root));
+    setRootVisible(false);
+    setShowsRootHandles(true);
+    setCellRenderer(new CatalogCellRenderer());
   }
 
+  /**
+   * Returns the furniture currently selected in the tree.
+   * @return an array of furniture. If no furniture is selected, the array length is 0.
+   */
   public PieceOfFurniture [] getSelectedFurniture() {
-    // TODO Auto-generated method stub
-    return null;
+    TreePath [] selectionPaths  = getSelectionPaths();
+    List<PieceOfFurniture> selectedFurniture = new ArrayList<PieceOfFurniture>();
+    for (TreePath path : selectionPaths) {
+      // Add to selectedFurniture all the nodes that matches a piece of furniture
+      if (path.getPathCount() == 3) {
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode)path.getLastPathComponent();
+        selectedFurniture.add((PieceOfFurniture)node.getUserObject());
+      }        
+    }
+    return selectedFurniture.toArray(new PieceOfFurniture [selectedFurniture.size()]);
   }  
+  
+  private class CatalogCellRenderer extends DefaultTreeCellRenderer {
+    private static final int DEFAULT_ICON_HEIGHT = 32;
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, 
+        Object value, boolean selected, boolean expanded, 
+        boolean leaf, int row, boolean hasFocus) {
+      // Get default label with its icon, background and focus colors 
+      JLabel label = (JLabel)super.getTreeCellRendererComponent( 
+          tree, value, selected, expanded, leaf, row, hasFocus);
+      // Get the rendered node
+      DefaultMutableTreeNode node = (DefaultMutableTreeNode)value;
+      // If user's objet node is a category, change label text
+      if (node.getUserObject() instanceof Category) {
+        Category category = (Category)node.getUserObject();
+        label.setText(category.getName());
+      } 
+      // Else if user's objet node is a piece of furntiure, change label text and icon
+      else if (node.getUserObject() instanceof PieceOfFurniture) {
+        PieceOfFurniture piece = 
+            (PieceOfFurniture)node.getUserObject();
+        label.setText(piece.getName());
+        try {
+          // Read the icon of the piece 
+          InputStream iconStream = piece.getIcon().openStream();
+          BufferedImage image = ImageIO.read(iconStream);
+          iconStream.close();
+          // Scale the read icon  
+          int rowHeight = isFixedRowHeight() 
+                              ? getRowHeight() 
+                              : DEFAULT_ICON_HEIGHT;
+          int imageWidth = image.getWidth() * rowHeight 
+                                            / image.getHeight();
+          Image scaledImage = image.getScaledInstance(
+              imageWidth, rowHeight, Image.SCALE_SMOOTH);
+          label.setIcon(new ImageIcon (scaledImage));
+        } catch (IOException ex) {
+          // Too bad the icon can't be read
+          ex.printStackTrace();
+        }
+      }
+      return label;
+    }
+  }
 }
