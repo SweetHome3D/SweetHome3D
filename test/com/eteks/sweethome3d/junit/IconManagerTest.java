325f5e145d846e82a1b267c593581b63aba84223
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2006-05-05 20:27:27 +0000

    IconManager test
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@81 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

---
+++ test/com/eteks/sweethome3d/junit/IconManagerTest.java
@@ -0,0 +1,189 @@
+/*
+ * IconManagerTest.java 4 mai 2006
+ * 
+ * Copyright (c) 2006 Emmanuel PUYBARET / eTeks <info@eteks.com>. All Rights
+ * Reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package com.eteks.sweethome3d.junit;
+
+import java.awt.Component;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import junit.framework.TestCase;
+
+import com.eteks.sweethome3d.model.Content;
+import com.eteks.sweethome3d.swing.IconManager;
+import com.eteks.sweethome3d.tools.URLContent;
+
+/**
+ * Tests IconManager class.
+ * @author Emmanuel Puybaret
+ */
+public class IconManagerTest extends TestCase {
+  private final int HEIGHT = 32;
+  
+  public void testIconManager() 
+      throws NoSuchFieldException, IllegalAccessException, InterruptedException, BrokenBarrierException, ClassNotFoundException {
+    // Test icon loading on a good image
+    testIconLoading(getClass().getResource("resources/test.png"), false);
+    // Test icon loading on a content that doesn't an image
+    testIconLoading(getClass().getResource("IconManagerTest.class"), false);
+
+    IconManager iconManager = IconManager.getInstance();
+    Class iconProxyClass = Class.forName(iconManager.getClass().getName() + "$IconProxy");
+    URLContent waitIconContent = (URLContent)getFieldValue(iconManager, "waitIconContent");
+    URLContent errorIconContent = (URLContent)getFieldValue(iconManager, "errorIconContent");
+
+    // Check waitIcon is loaded directly without proxy
+    Icon waitIcon = iconManager.getIcon(waitIconContent, HEIGHT, null);
+    assertNotSame("Wait icon loaded with IconProxy", waitIcon.getClass(), iconProxyClass);
+
+    // Check errorIcon is loaded directly without proxy
+    Icon errorIcon = iconManager.getIcon(errorIconContent, HEIGHT, null);
+    assertNotSame("Error icon loaded with IconProxy", errorIcon.getClass(), iconProxyClass);
+  }
+
+  /**
+   * Test how an icon is loaded by IconManager. 
+   */
+  private void testIconLoading(URL iconURL, boolean goodIcon) 
+      throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, InterruptedException, BrokenBarrierException {
+    IconManager iconManager = IconManager.getInstance();
+    Class iconProxyClass = Class.forName(iconManager.getClass().getName() + "$IconProxy");
+    
+    URLContent waitIconContent = (URLContent)getFieldValue(iconManager, "waitIconContent");
+    URLContent errorIconContent = (URLContent)getFieldValue(iconManager, "errorIconContent");
+    
+    final CyclicBarrier waintingComponentBarrier = new CyclicBarrier(2);
+    // A dummy waiting component that waits on a barrier in its repaint method 
+    Component waitingComponent = new Component() { 
+      public void repaint() {
+        awaitBarrier(waintingComponentBarrier); 
+      }
+    };
+    
+    Content iconContent = new URLContent(iconURL);
+    // Add a barrier in iconsLoader of iconManager to avoid immediat loading of iconContent
+    CyclicBarrier iconsLoaderBarrier = createBarrierInIconManager(iconManager);
+    Icon icon = iconManager.getIcon(iconContent, HEIGHT, waitingComponent);
+    assertEquals("Icon not equal to wait icon while loading", waitIconContent.getURL(), icon);
+
+    // Let iconsLoader of iconManager load the iconContent
+    iconsLoaderBarrier.await();
+    // Wait iconContent loading completion
+    waintingComponentBarrier.await();
+    if (goodIcon) {
+      assertEquals("Icon not equal to icon read from resource", iconURL, icon);
+    } else {
+      assertEquals("Wrong icon not equal to errorIcon", errorIconContent.getURL(), icon);
+    }
+    
+    // Check icon is loaded with proxy
+    assertSame("Icon not loaded with IconProxy", icon.getClass(), iconProxyClass);
+
+    // Check that icon is stored in cache
+    Icon iconFromCache = iconManager.getIcon(iconContent, HEIGHT, waitingComponent);
+    assertSame("Test icon reloaded", icon, iconFromCache);
+  }
+  
+  /**
+   * Returns a barrier that blocks the loading task of icons in iconManager.
+   */
+  private CyclicBarrier createBarrierInIconManager(IconManager iconManager) 
+        throws NoSuchFieldException, IllegalAccessException {
+    final CyclicBarrier barrier = new CyclicBarrier(2);
+    // Set maximum pool size of iconsLoader in iconManager to 1
+    // so we can control the task it will run with a barrier in a RejectedExecutionHandler instance
+    ThreadPoolExecutor iconsLoader = (ThreadPoolExecutor)getFieldValue(iconManager, "iconsLoader");
+    iconsLoader.setMaximumPoolSize(1);
+    // Execute a dummy task that will reject the task executed by iconsLoader
+    iconsLoader.execute(new Runnable() {
+        public void run() {
+          awaitBarrier(barrier); // First meeting
+          barrier.reset();
+          awaitBarrier(barrier); // Second meeting
+        }
+      });
+    // Set a RejectedExecutionHandler to control the launch of a task executed
+    // by iconsLoader from test thread
+    iconsLoader.setRejectedExecutionHandler(new RejectedExecutionHandler () {
+        public void rejectedExecution(final Runnable r, ThreadPoolExecutor executor) {
+          awaitBarrier(barrier); // Second meeting
+          barrier.reset();
+          executor.execute(new Runnable() {
+            public void run() {
+              awaitBarrier(barrier); // Third meeting
+              r.run();
+            }
+          });
+        }
+      });
+    // Ensure the dummy task is launched
+    awaitBarrier(barrier); // First meeting
+    return barrier;
+  }
+  
+  private void awaitBarrier(CyclicBarrier barrier) {
+    try {
+      barrier.await();
+    } catch (Exception ex) {
+      fail();
+    }
+  }
+
+  /**
+   * Returns the value of <code>fieldName</code> in a given <code>instance</code> by reflection.
+   */
+  private Object getFieldValue(Object instance, String fieldName)
+      throws NoSuchFieldException, IllegalAccessException {
+    Field field = instance.getClass().getDeclaredField(fieldName);
+    field.setAccessible(true);
+    return field.get(instance);
+  }
+
+  /**
+   * Asserts icons in parameter at same size contains the same image data. 
+   */
+  private void assertEquals(String message, URL expectedIconURL, Icon actualIcon) {
+    ImageIcon expectedIcon = new ImageIcon(expectedIconURL);
+    Image scaledExpectedImage = expectedIcon.getImage()
+        .getScaledInstance(actualIcon.getIconWidth(),
+            actualIcon.getIconHeight(), Image.SCALE_SMOOTH);
+    assertTrue(message, Arrays.equals(getIconData(new ImageIcon(scaledExpectedImage)), 
+                                      getIconData(actualIcon)));
+  }
+
+  private int [] getIconData(Icon icon) {
+    BufferedImage image = new BufferedImage(icon.getIconWidth(),
+        icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+    icon.paintIcon(null, image.getGraphics(), 0, 0);
+    int [] imageData = new int [icon.getIconWidth() * icon.getIconHeight()];
+    return image.getRGB(0, 0, icon.getIconWidth(), icon.getIconHeight(), 
+        imageData, 0, icon.getIconWidth());
+  }
+}
