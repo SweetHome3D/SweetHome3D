ebd6167dcf89f28c007a8b25943e6d258d044b0c
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2006-06-10 14:45:35 +0000

    Optimized imports
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@172 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/swing/FurnitureController.java
+++ src/com/eteks/sweethome3d/swing/FurnitureController.java
@@ -19,9 +19,7 @@
  */
 package com.eteks.sweethome3d.swing;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
