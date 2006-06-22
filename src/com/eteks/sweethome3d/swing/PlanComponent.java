e81f5dbc9e4b08e3e2b013cf4e74393028c26ced
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2006-06-22 09:27:26 +0000

    Optimzed imports
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@200 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/swing/PlanComponent.java
+++ src/com/eteks/sweethome3d/swing/PlanComponent.java
@@ -46,9 +46,7 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.swing.JComponent;
 import javax.swing.UIManager;
