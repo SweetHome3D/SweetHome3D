bdeedb548e184f9fd19305210a6b24b310859b9b
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2006-09-01 17:34:10 +0000

    Factorized ControllerAction instantiations code
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@327 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/swing/HomePane.java
+++ src/com/eteks/sweethome3d/swing/HomePane.java
@@ -20,129 +20,91 @@
 package com.eteks.sweethome3d.swing;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
 import java.util.ResourceBundle;
 
-import javax.swing.AbstractButton;
 import javax.swing.Action;
 import javax.swing.ActionMap;
-import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JComponent;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JPopupMenu;
 import javax.swing.JRootPane;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
-import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
 
 import com.eteks.sweethome3d.model.Home;
 import com.eteks.sweethome3d.model.UserPreferences;
 
 /**
- * The MVC view that edits home. 
+* The MVC view that edits home. 
  * @author Emmanuel Puybaret
  */
 public class HomePane extends JRootPane {
-  private JCheckBoxMenuItem wallCreationCheckBoxMenuItem;
-  private JToggleButton     wallCreationToggleButton;
-
   public enum ActionType {
-    ADD_HOME_FURNITURE, DELETE_HOME_FURNITURE, UNDO, REDO, WALL_CREATION, DELETE_SELECTION}
-
+    ADD_HOME_FURNITURE, DELETE_HOME_FURNITURE, UNDO, REDO}
+  
   /**
-   * Create this view associated with its controller.
+  * Create this view associated with its controller.
    */
   public HomePane(Home home, UserPreferences preferences, HomeController controller) {
-    JPopupMenu.setDefaultLightWeightPopupEnabled(false);
     createActions(controller);
     setJMenuBar(getHomeMenuBar());
     getContentPane().add(getToolBar(), BorderLayout.NORTH);
-    getContentPane().add(getMainPane(home, preferences, controller));
+    getContentPane().add(getCatalogFurniturePane(home, preferences));
   }
   
+  /**
+   * Create the actions map of this component.
+   */
   private void createActions(final HomeController controller) {
-    ResourceBundle resource = ResourceBundle.getBundle(
-        HomePane.class.getName());
-    ActionMap actions = getActionMap();    
     try {
-      actions.put(ActionType.ADD_HOME_FURNITURE,
-          new ControllerAction(resource, ActionType.ADD_HOME_FURNITURE.toString(),
-              controller, "addHomeFurniture"));
-      actions.put(ActionType.DELETE_HOME_FURNITURE,
-          new ControllerAction(resource, ActionType.DELETE_HOME_FURNITURE.toString(),
-              controller, "deleteHomeFurniture"));
-      actions.put(ActionType.UNDO,
-          new ControllerAction(resource, ActionType.UNDO.toString(),
-              controller, "undo"));
-      actions.put(ActionType.REDO,
-          new ControllerAction(resource, ActionType.REDO.toString(),
-              controller, "redo"));
-      actions.put(ActionType.WALL_CREATION,
-          new ResourceAction (resource, ActionType.WALL_CREATION.toString()) {
-            public void actionPerformed(ActionEvent ev) {
-              boolean selected = ((AbstractButton)ev.getSource()).isSelected();
-              if (selected) {
-                controller.setWallCreationMode();
-              } else {
-                controller.setSelectionMode();
-              }
-              // Update selected state of tool bar button and menu item
-              wallCreationToggleButton.setSelected(selected);
-              wallCreationCheckBoxMenuItem.setSelected(selected);
-            }
-          });
-      actions.put(ActionType.DELETE_SELECTION, 
-          new ControllerAction(resource, ActionType.DELETE_SELECTION.toString(),
-              controller.getPlanController(), "deleteSelection"));
+      createAction(ActionType.UNDO, controller, "undo");
+      createAction(ActionType.REDO, controller, "redo");
+      createAction(ActionType.ADD_HOME_FURNITURE,
+          controller, "addHomeFurniture");
+      createAction(ActionType.DELETE_HOME_FURNITURE,
+          controller, "deleteHomeFurniture");
     } catch (NoSuchMethodException ex) {
       throw new RuntimeException(ex);
     }
   }
+
+  private void createAction(ActionType action, Object controller,
+                     String method) throws NoSuchMethodException {
+    ResourceBundle resource = ResourceBundle.getBundle(
+          HomePane.class.getName());
+    getActionMap().put(action, new ControllerAction(
+        resource, action.toString(), controller, method));
+  }
   
   /**
-   * Returns the menu bar displayed in this pane.
+  * Returns the menu bar displayed in this pane.
    */
   private JMenuBar getHomeMenuBar() {
     ResourceBundle resource = ResourceBundle.getBundle(
-        HomePane.class.getName());
+                                                       HomePane.class.getName());
     ActionMap actions = getActionMap();
-    
     // Create Edit menu
     JMenu editMenu = new JMenu(
-        new ResourceAction(resource, "EDIT_MENU"));
+                               new ResourceAction(resource, "EDIT_MENU"));
     editMenu.setEnabled(true);
     editMenu.add(actions.get(ActionType.UNDO));
     editMenu.add(actions.get(ActionType.REDO));
-    
     // Create Furniture menu
     JMenu furnitureMenu = new JMenu(
-        new ResourceAction(resource, "FURNITURE_MENU"));
+                                    new ResourceAction(resource, "FURNITURE_MENU"));
     furnitureMenu.setEnabled(true);
     furnitureMenu.add(actions.get(ActionType.ADD_HOME_FURNITURE));
     furnitureMenu.add(actions.get(ActionType.DELETE_HOME_FURNITURE));
-    
-    // Create Plan menu
-    JMenu planMenu = new JMenu(
-        new ResourceAction(resource, "PLAN_MENU"));
-    planMenu.setEnabled(true);
-    this.wallCreationCheckBoxMenuItem = 
-        new JCheckBoxMenuItem(actions.get(ActionType.WALL_CREATION));
-    planMenu.add(this.wallCreationCheckBoxMenuItem);
-    planMenu.add(actions.get(ActionType.DELETE_SELECTION));
-
     // Add menus to menu bar
     JMenuBar menuBar = new JMenuBar();
     menuBar.add(editMenu);
     menuBar.add(furnitureMenu);
-    menuBar.add(planMenu);
     return menuBar;
   }
-
+  
   /**
-   * Returns the tool bar displayed in this pane.
+  * Returns the tool bar displayed in this pane.
    */
   private JToolBar getToolBar() {
     JToolBar toolBar = new JToolBar();
@@ -150,28 +112,21 @@ public class HomePane extends JRootPane {
     toolBar.add(actions.get(ActionType.ADD_HOME_FURNITURE));
     toolBar.add(actions.get(ActionType.DELETE_HOME_FURNITURE));
     toolBar.addSeparator();
-    this.wallCreationToggleButton = 
-      new JToggleButton(actions.get(ActionType.WALL_CREATION));
-    // Don't display text with icon
-    this.wallCreationToggleButton.setText("");
-    toolBar.add(this.wallCreationToggleButton);
-    toolBar.add(actions.get(ActionType.DELETE_SELECTION));
-    toolBar.addSeparator();
     toolBar.add(actions.get(ActionType.UNDO));
     toolBar.add(actions.get(ActionType.REDO));
     return toolBar;
   }
   
   /**
-   * Enables or disables the action matching <code>actionType</code>.
+  * Enables or disables the action matching <code>actionType</code>.
    */
   public void setEnabled(ActionType actionType, 
                          boolean enabled) {
     getActionMap().get(actionType).setEnabled(enabled);
   }
-
+  
   /**
-   * Sets the <code>NAME</code> and <code>SHORT_DESCRIPTION</code> properties value 
+  * Sets the <code>NAME</code> and <code>SHORT_DESCRIPTION</code> properties value 
    * of undo and redo actions. If a parameter is null,
    * the properties will be reset to their initial values.
    */
@@ -179,9 +134,9 @@ public class HomePane extends JRootPane {
     setNameAndShortDescription(ActionType.UNDO, undoText);
     setNameAndShortDescription(ActionType.REDO, redoText);
   }
-
+  
   /**
-   * Sets the <code>NAME</code> and <code>SHORT_DESCRIPTION</code> properties value 
+  * Sets the <code>NAME</code> and <code>SHORT_DESCRIPTION</code> properties value 
    * matching <code>actionType</code>. If <code>name</code> is null,
    * the properties will be reset to their initial values.
    */
@@ -193,50 +148,19 @@ public class HomePane extends JRootPane {
     action.putValue(Action.NAME, name);
     action.putValue(Action.SHORT_DESCRIPTION, name);
   }
-
+  
   /**
-   * Returns the main pane with catalog tree, furniture table and plan pane. 
-   */
-  private JComponent getMainPane(Home home, UserPreferences preferences, 
-                                 HomeController controller) {
-    JSplitPane mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, 
-        getCatalogFurniturePane(home, preferences), 
-        getPlanView3DPane(home, controller));
-    mainPane.setContinuousLayout(true);
-    mainPane.setOneTouchExpandable(true);
-    mainPane.setResizeWeight(0.3);
-    return mainPane;
-  }
-
-  /**
-   * Returns the catalog tree and furniture table pane. 
+  * Returns the catalog tree and furniture table pane. 
    */
   private JComponent getCatalogFurniturePane(Home home, UserPreferences preferences) {
     JComponent catalogView = new CatalogTree(preferences.getCatalog());
     JComponent furnitureView = new FurnitureTable(home, preferences);
     // Create a split pane that displays both components
     JSplitPane catalogFurniturePane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, 
-        new JScrollPane(catalogView), new JScrollPane(furnitureView));
+                                                     new JScrollPane(catalogView), new JScrollPane(furnitureView));
     catalogFurniturePane.setContinuousLayout(true);
     catalogFurniturePane.setOneTouchExpandable(true);
     catalogFurniturePane.setResizeWeight(0.5);
     return catalogFurniturePane;
-  }
-
-  /**
-   * Returns the plan view and 3D view pane. 
-   */
-  private JComponent getPlanView3DPane(Home home, HomeController controller) {
-    JComponent planView = controller.getPlanController().getView();
-    JComponent view3D = new HomeComponent3D(home);
-    view3D.setPreferredSize(planView.getPreferredSize());
-    view3D.setMinimumSize(new Dimension(0, 0));
-    // Create a split pane that displays both components
-    JSplitPane planView3DPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, 
-        new JScrollPane(planView), view3D);
-    planView3DPane.setContinuousLayout(true);
-    planView3DPane.setOneTouchExpandable(true);
-    planView3DPane.setResizeWeight(0.5);
-    return planView3DPane;
   }
 }
