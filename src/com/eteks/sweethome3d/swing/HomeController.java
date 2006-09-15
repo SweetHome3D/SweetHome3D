14887c209a1cdf88642b449a10d0a98f07288a78
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2006-09-15 12:37:26 +0000

    Splited addHomeFurniture in two methods
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@374 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/swing/HomeController.java
+++ src/com/eteks/sweethome3d/swing/HomeController.java
@@ -37,9 +37,7 @@ import javax.swing.undo.UndoableEditSupport;
 
 import com.eteks.sweethome3d.model.CatalogPieceOfFurniture;
 import com.eteks.sweethome3d.model.Home;
-import com.eteks.sweethome3d.model.HomeApplication;
 import com.eteks.sweethome3d.model.HomePieceOfFurniture;
-import com.eteks.sweethome3d.model.RecorderException;
 import com.eteks.sweethome3d.model.SelectionEvent;
 import com.eteks.sweethome3d.model.SelectionListener;
 import com.eteks.sweethome3d.model.UserPreferences;
@@ -51,23 +49,10 @@ import com.eteks.sweethome3d.model.UserPreferences;
 public class HomeController {
   private Home                home;
   private UserPreferences     preferences;
-  private HomeApplication     application;
   private JComponent          homeView;
   private UndoableEditSupport undoSupport;
   private UndoManager         undoManager;
   private ResourceBundle      resource;
-  private int                 saveUndoLevel; 
-  
-  private PlanController      planController;
-  
-  /**
-   * Creates the controller of home view. 
-   * @param home        the home edited by this controller and its view.
-   * @param application the instance of current application.
-   */
-  public HomeController(Home home, HomeApplication application) {
-    this(home, application.getUserPreferences(), application);
-  }
 
   /**
    * Creates the controller of home view. 
@@ -75,23 +60,15 @@ public class HomeController {
    * @param preferences the preferences of the application.
    */
   public HomeController(Home home, UserPreferences preferences) {
-    this(home, preferences, null);
-  }
-
-  private HomeController(Home home, UserPreferences preferences, 
-                         HomeApplication application) {
     this.home = home;
     this.preferences = preferences;
-    this.application = application;
     this.undoSupport = new UndoableEditSupport();
     this.undoManager = new UndoManager();
     this.undoSupport.addUndoableEditListener(this.undoManager);
     this.resource = ResourceBundle.getBundle(
         HomeController.class.getName());
-    this.planController = new PlanController(home, preferences, undoSupport);
     this.homeView = new HomePane(home, preferences, this);
     addListeners();
-    enableDefaultActions();
   }
 
   /**
@@ -117,7 +94,9 @@ public class HomeController {
     this.preferences.getCatalog().addSelectionListener(
       new SelectionListener() {
         public void selectionChanged(SelectionEvent ev) {
-          enableActionsOnSelection();
+          ((HomePane)getView()).setEnabled(
+              HomePane.ActionType.ADD_HOME_FURNITURE,
+              !ev.getSelectedItems().isEmpty());
         }
       });
   }
@@ -128,36 +107,19 @@ public class HomeController {
   private void addHomeSelectionListener() {
     this.home.addSelectionListener(new SelectionListener() {
       public void selectionChanged(SelectionEvent ev) {
-        enableActionsOnSelection();
+        // Search if selection contains at least one piece
+        boolean selectionContainsFurniture = false;
+        for (Object item : ev.getSelectedItems()) {
+          if (item instanceof HomePieceOfFurniture) {
+            selectionContainsFurniture = true;
+            break;
+          }
+        }
+        ((HomePane)getView()).setEnabled(
+            HomePane.ActionType.DELETE_HOME_FURNITURE,
+            selectionContainsFurniture);
       }
     });
-  }
-  
-  /**
-   * Enables action bound to selection. 
-   */
-  private void enableActionsOnSelection() {
-    boolean wallCreationMode =  
-        this.planController.getMode() == PlanController.Mode.WALL_CREATION;
-    
-    // Search if selection contains at least one piece
-    List selectedItems = this.home.getSelectedItems();
-    boolean selectionContainsFurniture = false;
-    if (!wallCreationMode)
-      for (Object item : selectedItems) {
-        if (item instanceof HomePieceOfFurniture) {
-          selectionContainsFurniture = true;
-          break;
-        }
-      }
-    // In creation mode al actions bound to selection are disabled
-    HomePane view = ((HomePane)getView());
-    view.setEnabled(HomePane.ActionType.DELETE_HOME_FURNITURE,
-        !wallCreationMode && selectionContainsFurniture);
-    view.setEnabled(HomePane.ActionType.DELETE_SELECTION,
-        !wallCreationMode && !selectedItems.isEmpty());
-    view.setEnabled(HomePane.ActionType.ADD_HOME_FURNITURE,
-        !wallCreationMode && !this.preferences.getCatalog().getSelectedFurniture().isEmpty());
   }
 
   /**
@@ -168,45 +130,42 @@ public class HomeController {
       new UndoableEditListener () {
         public void undoableEditHappened(UndoableEditEvent ev) {
           HomePane view = ((HomePane)getView());
-          view.setEnabled(HomePane.ActionType.UNDO, 
-              planController.getMode() != PlanController.Mode.WALL_CREATION);
+          view.setEnabled(HomePane.ActionType.UNDO, true);
           view.setEnabled(HomePane.ActionType.REDO, false);
           view.setUndoRedoName(ev.getEdit().getUndoPresentationName(), null);
-          saveUndoLevel++;
-          home.setModified(true);
         }
       });
   }
 
   /**
-   * Enables actions at controller instantiation. 
-   */
-  private void enableDefaultActions() {
-    HomePane view = ((HomePane)getView());
-    view.setEnabled(HomePane.ActionType.NEW_HOME, true);
-    view.setEnabled(HomePane.ActionType.OPEN, true);
-    view.setEnabled(HomePane.ActionType.CLOSE, true);
-    view.setEnabled(HomePane.ActionType.SAVE, true);
-    view.setEnabled(HomePane.ActionType.SAVE_AS, true);
-    view.setEnabled(HomePane.ActionType.EXIT, true);
-    view.setEnabled(HomePane.ActionType.WALL_CREATION, true);
-  }
-
-  /**
-   * Controls new furniture added to home. Once added the furniture will be selected in view 
+   * Controls new furniture added to home from catalog. 
+   * Once added the furniture will be selected in view 
    * and undo support will receive a new undoable edit.
    */
   public void addHomeFurniture() {
-    List<CatalogPieceOfFurniture> furniture =
-        this.preferences.getCatalog().getSelectedFurniture();
-    final List<Object> oldSelection = this.home.getSelectedItems();
+    List<CatalogPieceOfFurniture> catalogFurniture =
+      this.preferences.getCatalog().getSelectedFurniture();
+    List<HomePieceOfFurniture> homeFurniture = 
+      new ArrayList<HomePieceOfFurniture>(catalogFurniture.size());
     // Create HomePieceOfFurniture instances that will be added to home
+    for (CatalogPieceOfFurniture piece : catalogFurniture) {
+      homeFurniture.add(new HomePieceOfFurniture(piece));
+    }
+    addFurniture(homeFurniture);
+  }
+  
+  /**
+   * Adds <code>furniture</code> to home. Once added the furniture will be selected in view 
+   * and undo support will receive a new undoable edit.
+   */
+  private void addFurniture(List<HomePieceOfFurniture> furniture) {
+    final List<Object> oldSelection = this.home.getSelectedItems();
     final HomePieceOfFurniture [] newFurniture = 
-      new HomePieceOfFurniture [furniture.size()];
+      furniture.toArray(new HomePieceOfFurniture [furniture.size()]);
+    // Get indices of furniture add to home
     final int [] furnitureIndex = new int [furniture.size()];
     int endIndex = home.getFurniture().size();
     for (int i = 0; i < furnitureIndex.length; i++) {
-      newFurniture [i] = new HomePieceOfFurniture(furniture.get(i));
       furnitureIndex [i] = endIndex++;
     }
 
@@ -308,8 +267,6 @@ public class HomeController {
     } else {
       view.setUndoRedoName(null, this.undoManager.getRedoPresentationName());
     }
-    this.saveUndoLevel--;
-    this.home.setModified(this.saveUndoLevel != 0);
   }
   
   /**
@@ -327,163 +284,6 @@ public class HomeController {
     } else {
       view.setUndoRedoName(this.undoManager.getUndoPresentationName(), null);
     }
-    this.saveUndoLevel++;
-    this.home.setModified(this.saveUndoLevel != 0);
-  }
-
-  /**
-   * Returns the controller of home plan.
-   */
-  public PlanController getPlanController() {
-    return this.planController;
-  }
-
-  /**
-   * Sets wall creation mode in plan controller, 
-   * and disables forbidden actions in this mode.  
-   */
-  public void setWallCreationMode() {
-    this.planController.setMode(PlanController.Mode.WALL_CREATION);
-    enableActionsOnSelection();
-    HomePane view = ((HomePane)getView());
-    view.setEnabled(HomePane.ActionType.UNDO, false);
-    view.setEnabled(HomePane.ActionType.REDO, false);
-  }
-
-  /**
-   * Sets wall creation mode in plan controller, 
-   * and enables authorized actions in this mode.  
-   */
-  public void setSelectionMode() {
-    this.planController.setMode(PlanController.Mode.SELECTION);
-    enableActionsOnSelection();
-    HomePane view = ((HomePane)getView());
-    view.setEnabled(HomePane.ActionType.UNDO, this.undoManager.canUndo());
-    view.setEnabled(HomePane.ActionType.REDO, this.undoManager.canRedo());
-  }
-
-  /**
-   * Creates a new home and adds it to application home list.
-   */
-  public void newHome() {
-    this.application.addHome(
-        new Home(application.getUserPreferences().getDefaultWallHeight()));
-  }
-
-  /**
-   * Opens a home. This method displays an {@link HomePane#showOpenDialog() open dialog} 
-   * in view, reads the home from the choosen name and adds it to application home list.
-   */
-  public void open() {
-    final String homeName = ((HomePane)getView()).showOpenDialog();
-    if (homeName != null) {
-      try {
-        Home openedHome = application.getHomeRecorder().readHome(homeName); 
-        application.addHome(openedHome);
-      } catch (RecorderException ex) {
-        String message = String.format(resource.getString("openError"), homeName);
-        ((HomePane)getView()).showError(message);
-      }
-    }
-  }
-
-  /**
-   * Manages home close operation. If the home managed by this controller is modified,
-   * this method will {@link HomePane#confirmSave(String) confirm} 
-   * in view whether home should be saved. Once home is actually saved,
-   * home is removed from application homes list.
-   */
-  public void close() {
-    boolean willClose = true;
-    if (this.home.isModified()) {
-      switch (((HomePane)getView()).confirmSave(this.home.getName())) {
-        case SAVE   : willClose = save();
-                      break;
-        case CANCEL : willClose = false;
-                      break;
-      }  
-    }
-    if (willClose) {
-      this.application.deleteHome(home);
-    }
-  }
-
-  /**
-   * Saves the home managed by this controller. If home name doesn't exist, 
-   * this method will act as {@link #saveAs() saveAs} method.
-   * @return <code>true</code> if home was saved.
-   */
-  public boolean save() {
-    if (this.home.getName() == null) {
-      return saveAs();
-    } else {
-      return save(this.home.getName());
-    }
-  }
-
-  /**
-   * Saves the home managed by this controller with a different name. 
-   * This method displays a {@link HomePane#showSaveDialog(String) save dialog} in   view, 
-   * and saves home with the choosen name if any. 
-   * If this name already exists, the user will be 
-   * {@link HomePane#confirmOverwrite(String) prompted} in view whether 
-   * he wants to overwrite this existing name. 
-   * @return <code>true</code> if home was saved.
-   */
-  public boolean saveAs() {
-    String newName = ((HomePane)getView()).showSaveDialog(this.home.getName());
-    if (newName != null) {
-      try {
-        if (!this.application.getHomeRecorder().exists(newName)
-            || ((HomePane)getView()).confirmOverwrite(newName)) {
-          return save(newName);
-        } else {
-          return saveAs();
-        }
-      } catch (RecorderException ex) {
-        String message = String.format(this.resource.getString("saveError"), newName);
-        ((HomePane)getView()).showError(message);
-      }
-    }
-    return false;
-  }
-
-  /**
-   * Actually saves the home managed by this controller.
-   * @return <code>true</code> if home was saved.
-   */
-  private boolean save(String homeName) {
-    try {
-      this.application.getHomeRecorder().writeHome(this.home, homeName);
-      this.saveUndoLevel = 0;
-      this.home.setModified(false);
-      return true;
-    } catch (RecorderException ex) {
-      String message = String.format(this.resource.getString("saveError"), homeName);
-      ((HomePane)getView()).showError(message);
-      return false;
-    }
-  }
-
-  /**
-   * Manages application exit. If any home in application homes list is modified,
-   * the user will {@link HomePane#confirmExit() prompted} in view whether he wants
-   * to discard his modifications.  
-   */
-  public void exit() {
-    for (Home home : this.application.getHomes()) {
-      if (home.isModified()) {
-        if (((HomePane)getView()).confirmExit()) {
-          break;
-        } else {
-          return;
-        }
-      }
-    }
-    // Remove all homes from application
-    for (Home home : this.application.getHomes()) {
-      this.application.deleteHome(home);
-    }
-    System.exit(0);
   }
 }
+
