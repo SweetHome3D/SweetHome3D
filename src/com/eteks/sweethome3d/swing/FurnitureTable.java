e601451f3591572ce9cb1d7e559981f91d99db6b
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2006-10-06 18:55:08 +0000

    Simplified nameRenderer
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@474 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/swing/FurnitureTable.java
+++ src/com/eteks/sweethome3d/swing/FurnitureTable.java
@@ -24,28 +24,17 @@ import static com.eteks.sweethome3d.model.UserPreferences.Unit.centimerToInch;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Rectangle;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.swing.JLabel;
 import javax.swing.JTable;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 
 import com.eteks.sweethome3d.model.Content;
-import com.eteks.sweethome3d.model.FurnitureEvent;
-import com.eteks.sweethome3d.model.FurnitureListener;
 import com.eteks.sweethome3d.model.Home;
 import com.eteks.sweethome3d.model.HomePieceOfFurniture;
-import com.eteks.sweethome3d.model.SelectionEvent;
-import com.eteks.sweethome3d.model.SelectionListener;
 import com.eteks.sweethome3d.model.UserPreferences;
 
 /**
@@ -53,8 +42,6 @@ import com.eteks.sweethome3d.model.UserPreferences;
  * @author Emmanuel Puybaret
  */
 public class FurnitureTable extends JTable {
-  private ListSelectionListener tableSelectionListener;
-
   /**
    * Creates a table that displays furniture of <code>home</code>.
    * @param home        the home displayed by this view
@@ -64,78 +51,6 @@ public class FurnitureTable extends JTable {
     String [] columnNames = getColumnNames();
     setModel(new FurnitureTableModel(home, columnNames));
     setColumnRenderers(preferences);
-    addSelectionListeners(home);
-    addUserPreferencesListener(preferences);
-  }
-  
-  /**
-   * Adds selection listeners to this table.
-   */
-  private void addSelectionListeners(final Home home) {   
-    final SelectionListener homeSelectionListener  = 
-      new SelectionListener() {
-        public void selectionChanged(SelectionEvent ev) {
-          getSelectionModel().removeListSelectionListener(tableSelectionListener);
-          List<HomePieceOfFurniture> furniture = home.getFurniture();
-          clearSelection();
-          for (Object item : ev.getSelectedItems()) {
-            if (item instanceof HomePieceOfFurniture) {
-              int index = furniture.indexOf(item); 
-              addRowSelectionInterval(index, index);
-              makeRowVisible(index);
-            }          
-          }        
-          getSelectionModel().addListSelectionListener(tableSelectionListener);
-        }
-      };
-    this.tableSelectionListener = 
-      new ListSelectionListener () {
-        public void valueChanged(ListSelectionEvent ev) {
-          if (!ev.getValueIsAdjusting()) {
-            home.removeSelectionListener(homeSelectionListener);
-            int [] selectedRows = getSelectedRows();
-            // Build the list of selected furniture
-            List<HomePieceOfFurniture> selectedFurniture =
-              new ArrayList<HomePieceOfFurniture>(selectedRows.length);
-            List<HomePieceOfFurniture> furniture = home.getFurniture();
-            for (int index : selectedRows) {
-              selectedFurniture.add(furniture.get(index));
-            }
-            // Set the new selection in home
-            home.setSelectedItems(selectedFurniture);
-            home.addSelectionListener(homeSelectionListener);
-          }
-        }
-      };
-    getSelectionModel().addListSelectionListener(this.tableSelectionListener);
-    home.addSelectionListener(homeSelectionListener);
-  }
-
-  /**
-   * Adds a listener to <code>preferences</code> to repaint this table
-   * when unit changes.  
-   */
-  private void addUserPreferencesListener(UserPreferences preferences) {
-    preferences.addPropertyChangeListener("unit", 
-      new PropertyChangeListener() {
-        public void propertyChange(PropertyChangeEvent ev) {
-          repaint();
-        }
-      });
-  }
-
-  /**
-   * Ensures the rectangle which displays row is visible.
-   */
-  private void makeRowVisible(int row) {
-    // Compute the rectangle that includes a row 
-    Rectangle includingRectangle = getCellRect(row, 0, true);
-    if (getAutoResizeMode() == AUTO_RESIZE_OFF) {
-      int lastColumn = getColumnCount() - 1;
-      includingRectangle = includingRectangle.
-          union(getCellRect(row, lastColumn, true));
-    }
-    scrollRectToVisible(includingRectangle);
   }
 
   /**
@@ -191,7 +106,7 @@ public class FurnitureTable extends JTable {
          table, piece.getName(), isSelected, hasFocus, row, column); 
        Content iconContent = piece.getIcon(); 
         label.setIcon(IconManager.getInstance().getIcon(
-            iconContent, getRowHeight(), FurnitureTable.this)); 
+            iconContent, getRowHeight(), table)); 
         return label;
       }
     };
@@ -238,7 +153,7 @@ public class FurnitureTable extends JTable {
       } 
     };
   }
- 
+
   /**
    * Model used by this table
    */
@@ -249,23 +164,6 @@ public class FurnitureTable extends JTable {
     public FurnitureTableModel(Home home, String [] columnNames) {
       this.home = home;
       this.columnNames = columnNames;
-      addHomeListener(home);
-    }
-
-    private void addHomeListener(Home home) {
-      home.addFurnitureListener(new FurnitureListener() {
-        public void pieceOfFurnitureChanged(FurnitureEvent ev) {
-          int pieceIndex = ev.getIndex();
-          switch (ev.getType()) {
-            case ADD :
-              fireTableRowsInserted(pieceIndex, pieceIndex);
-              break;
-            case DELETE :
-              fireTableRowsDeleted(pieceIndex, pieceIndex);
-              break;
-          }
-        }
-      });
     }
 
     @Override
