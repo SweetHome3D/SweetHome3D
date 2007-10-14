9da9c47861d9c2bbcd17149ba309da737bae943d
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2007-10-14 00:09:31 +0000

    Put Sweet Home 3D version in a property
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@990 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/swing/HomePane.java
+++ src/com/eteks/sweethome3d/swing/HomePane.java
@@ -1328,7 +1328,8 @@ public class HomePane extends JRootPane {
    */
   public void showAboutDialog() {
     String messageFormat = this.resource.getString("about.message");
-    String message = String.format(messageFormat, System.getProperty("java.version"));
+    String aboutVersion = this.resource.getString("about.version");
+    String message = String.format(messageFormat, aboutVersion, System.getProperty("java.version"));
     // Use an uneditable editor pane to let user select text in dialog
     JEditorPane messagePane = new JEditorPane("text/html", message);
     messagePane.setOpaque(false);
