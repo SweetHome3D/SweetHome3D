6acac868dfd11e4ed8f7e416b9bdfd26e1cf3a4c
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2008-10-31 06:46:36 +0000

    Updated javadoc
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@1445 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/viewcontroller/WallController.java
+++ src/com/eteks/sweethome3d/viewcontroller/WallController.java
@@ -160,14 +160,14 @@ public class WallController implements Controller {
   }
 
   /**
-   * Adds the property change <code>listener</code> in parameter to this home.
+   * Adds the property change <code>listener</code> in parameter to this controller.
    */
   public void addPropertyChangeListener(Property property, PropertyChangeListener listener) {
     this.propertyChangeSupport.addPropertyChangeListener(property.toString(), listener);
   }
 
   /**
-   * Removes the property change <code>listener</code> in parameter from this home.
+   * Removes the property change <code>listener</code> in parameter from this controller.
    */
   public void removePropertyChangeListener(Property property, PropertyChangeListener listener) {
     this.propertyChangeSupport.removePropertyChangeListener(property.toString(), listener);
