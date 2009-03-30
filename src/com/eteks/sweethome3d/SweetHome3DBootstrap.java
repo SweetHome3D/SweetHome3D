7b6ccb7c77e12407dcdecdc120db73f48a0e2cd1
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2009-03-30 16:18:37 +0000

    Updated to iText to version 2.1.5
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@1848 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
+++ src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
@@ -51,7 +51,7 @@ public class SweetHome3DBootstrap {
         "libjogl.jnilib",
         "libjogl_awt.jnilib",
         "libjogl_cg.jnilib",
-        "iText-2.1.2u.jar", // Jars included in Sweet Home 3D executable jar file 
+        "iText-2.1.5.jar", // Jars included in Sweet Home 3D executable jar file 
         "freehep-vectorgraphics-svg-2.1.1.jar",
         "Loader3DS1_2.jar",
         "jnlp.jar"};
