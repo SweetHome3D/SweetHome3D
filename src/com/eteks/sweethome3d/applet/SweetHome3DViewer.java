185c73d5d166f8b012920144cb2c4e433e1653e1
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2009-03-31 12:40:49 +0000

    Added Sweet Home 3D viewer applet
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@1863 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

---
+++ src/com/eteks/sweethome3d/applet/SweetHome3DViewer.java
@@ -0,0 +1,156 @@
+/*
+ * SweetHome3DApplet.java 10 oct. 2008
+ *
+ * Copyright (c) 2008 Emmanuel PUYBARET / eTeks <info@eteks.com>. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.eteks.sweethome3d.applet;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.JApplet;
+import javax.swing.JLabel;
+
+import com.eteks.sweethome3d.tools.ExtensionsClassLoader;
+
+/**
+ * This applet class displays the 3D view of a Sweet Home 3D file from its URL.
+ * <p>This applet accepts the following parameter:
+ * 
+ * <ul><li><code>homeURL</code> specifies the URL of the home that will be downloaded 
+ *     and displayed at applet launch. 
+ *     <br>This URL should return a content at SH3D file format. If it's not an absolute URL, 
+ *     it will be read relative to applet document base. By default, this parameter is 
+ *     equal to <code>default.sh3d</code>.</li></ul>
+ *     
+ * <p>Use space bar to switch between aerial view and virtual visitor view</p>.
+ * 
+ * <p>The bytecode of this class is Java 1.1 compatible and to be able to notify users that 
+ * it requires Java 5 when it's run under an old JVM.
+ *     
+ * @author Emmanuel Puybaret
+ */
+public class SweetHome3DViewer extends JApplet {
+  private Object appletApplication;
+  
+  public void init() {
+    if (!isJava5OrSuperior()) {
+      showError("<html><p>This applet may be run under Windows, Mac OS X 10.4 / 10.5, Linux and Solaris." +
+          "<br>It requires Java version 5 or superior.</p>" +
+          "<p>Please, check Java version set in Java preferences under Mac OS X," +
+          "<br>or update your Java Runtime to the latest version available at java.com under the other systems.</p>");
+    } else {
+      createAppletApplication();
+    }
+  }
+
+  public void destroy() {
+    if (this.appletApplication != null) {
+      try {
+        Method destroyMethod = this.appletApplication.getClass().getMethod("destroy", new Class [0]);
+        destroyMethod.invoke(this.appletApplication, new Object [0]);
+      } catch (Exception ex) {
+        // Can't do better than print stack trace when applet is destroyed
+        ex.printStackTrace();
+      }
+    }
+    this.appletApplication = null;
+    System.gc();
+  }
+  
+  /**
+   * Returns <code>true</code> if current JVM version is 5+. 
+   */
+  private boolean isJava5OrSuperior() {
+    String javaVersion = System.getProperty("java.version");
+    String [] javaVersionParts = javaVersion.split("\\.|_");
+    if (javaVersionParts.length >= 1) {
+      try {
+        // Return true for Java SE 5 and superior
+        if (Integer.parseInt(javaVersionParts [1]) >= 5) {
+          return true;
+        }
+      } catch (NumberFormatException ex) {
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Shows the given text in a label.
+   */
+  private void showError(String text) {
+    JLabel label = new JLabel(text, JLabel.CENTER);
+    setContentPane(label);
+  }
+  
+  /**
+   * Creates a new <code>AppletApplication</code> instance that manages this applet content.
+   */
+  private void createAppletApplication() {
+    try {
+      Class sweetHome3DAppletClass = SweetHome3DViewer.class;
+      String [] java3DFiles = {
+          "j3dcore.jar", // Main Java 3D jars
+          "vecmath.jar",
+          "j3dutils.jar",
+          "j3dcore-d3d.dll", // Windows DLLs
+          "j3dcore-ogl.dll",
+          "j3dcore-ogl-cg.dll",
+          "j3dcore-ogl-chk.dll",
+          "libj3dcore-ogl.so", // Linux DLLs
+          "libj3dcore-ogl-cg.so",
+          "gluegen-rt.jar", // Mac OS X jars and DLLs
+          "jogl.jar",
+          "libgluegen-rt.jnilib",
+          "libjogl.jnilib",
+          "libjogl_awt.jnilib",
+          "libjogl_cg.jnilib"};
+      List applicationPackages = new ArrayList(Arrays.asList(new String [] {
+          "com.eteks.sweethome3d",
+          "javax.media.j3d",
+          "javax.vecmath",
+          "com.sun.j3d",
+          "com.sun.opengl",
+          "com.sun.gluegen.runtime",
+          "javax.media.opengl",
+          "com.microcrowd.loader.java3d"}));
+      
+      ClassLoader extensionsClassLoader = new ExtensionsClassLoader(
+          sweetHome3DAppletClass.getClassLoader(), sweetHome3DAppletClass.getProtectionDomain(),
+          java3DFiles, (String [])applicationPackages.toArray(new String [applicationPackages.size()]));
+      
+      // Call application constructor with reflection
+      String applicationClassName = "com.eteks.sweethome3d.applet.ViewerHelper";
+      Class applicationClass = extensionsClassLoader.loadClass(applicationClassName);
+      Constructor applicationConstructor = 
+          applicationClass.getConstructor(new Class [] {JApplet.class});
+      this.appletApplication = applicationConstructor.newInstance(new Object [] {this});
+    } catch (Throwable ex) {
+      if (ex instanceof InvocationTargetException) {
+        ex = ((InvocationTargetException)ex).getCause();
+      }
+      showError("<html>Can't start applet:<br>Exception" 
+          + ex.getClass().getName() + " " + ex.getMessage());
+      ex.printStackTrace();
+    }
+  }  
+}
---
+++ src/com/eteks/sweethome3d/applet/ViewerHelper.java
@@ -0,0 +1,382 @@
+/*
+ * ViewerHelper.java 31 mars 2009
+ *
+ * Copyright (c) 2009 Emmanuel PUYBARET / eTeks <info@eteks.com>. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.eteks.sweethome3d.applet;
+
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.Callable;
+
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JApplet;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+
+import com.eteks.sweethome3d.io.DefaultHomeInputStream;
+import com.eteks.sweethome3d.j3d.Component3DManager;
+import com.eteks.sweethome3d.j3d.TextureManager;
+import com.eteks.sweethome3d.model.BackgroundImage;
+import com.eteks.sweethome3d.model.CatalogPieceOfFurniture;
+import com.eteks.sweethome3d.model.CatalogTexture;
+import com.eteks.sweethome3d.model.FurnitureCatalog;
+import com.eteks.sweethome3d.model.Home;
+import com.eteks.sweethome3d.model.InterruptedRecorderException;
+import com.eteks.sweethome3d.model.RecorderException;
+import com.eteks.sweethome3d.model.UserPreferences;
+import com.eteks.sweethome3d.swing.HomeComponent3D;
+import com.eteks.sweethome3d.swing.ThreadedTaskPanel;
+import com.eteks.sweethome3d.viewcontroller.BackgroundImageWizardController;
+import com.eteks.sweethome3d.viewcontroller.DialogView;
+import com.eteks.sweethome3d.viewcontroller.FurnitureCatalogController;
+import com.eteks.sweethome3d.viewcontroller.FurnitureController;
+import com.eteks.sweethome3d.viewcontroller.HelpController;
+import com.eteks.sweethome3d.viewcontroller.HelpView;
+import com.eteks.sweethome3d.viewcontroller.Home3DAttributesController;
+import com.eteks.sweethome3d.viewcontroller.HomeController;
+import com.eteks.sweethome3d.viewcontroller.HomeController3D;
+import com.eteks.sweethome3d.viewcontroller.HomeFurnitureController;
+import com.eteks.sweethome3d.viewcontroller.HomeView;
+import com.eteks.sweethome3d.viewcontroller.ImportedFurnitureWizardController;
+import com.eteks.sweethome3d.viewcontroller.ImportedFurnitureWizardStepsView;
+import com.eteks.sweethome3d.viewcontroller.ImportedTextureWizardController;
+import com.eteks.sweethome3d.viewcontroller.LabelController;
+import com.eteks.sweethome3d.viewcontroller.PageSetupController;
+import com.eteks.sweethome3d.viewcontroller.PlanController;
+import com.eteks.sweethome3d.viewcontroller.PlanView;
+import com.eteks.sweethome3d.viewcontroller.PrintPreviewController;
+import com.eteks.sweethome3d.viewcontroller.RoomController;
+import com.eteks.sweethome3d.viewcontroller.TextureChoiceController;
+import com.eteks.sweethome3d.viewcontroller.TextureChoiceView;
+import com.eteks.sweethome3d.viewcontroller.ThreadedTaskController;
+import com.eteks.sweethome3d.viewcontroller.ThreadedTaskView;
+import com.eteks.sweethome3d.viewcontroller.UserPreferencesController;
+import com.eteks.sweethome3d.viewcontroller.View;
+import com.eteks.sweethome3d.viewcontroller.ViewFactory;
+import com.eteks.sweethome3d.viewcontroller.WallController;
+import com.eteks.sweethome3d.viewcontroller.WizardController;
+
+/**
+ * Helper for {@link SweetHome3DViewer SweetHome3DViewer}. This class is public 
+ * because it's loaded by applet viewer class loader as a start point.
+ * @author Emmanuel Puybaret
+ */
+public final class ViewerHelper {
+  private static final String HOME_URL_PARAMETER = "homeURL";
+  
+  public ViewerHelper(final JApplet applet) {
+    // Create default user preferences with no catalog
+    final UserPreferences preferences = new UserPreferences() {
+        @Override
+        public void addFurnitureLibrary(String furnitureLibraryName) throws RecorderException {
+          throw new UnsupportedOperationException();
+        }
+  
+        @Override
+        public boolean furnitureLibraryExists(String furnitureLibraryName) throws RecorderException {
+          throw new UnsupportedOperationException();
+        }
+  
+        @Override
+        public void write() throws RecorderException {
+          throw new UnsupportedOperationException();
+        }
+      };
+    
+    // Create a view factory able to instantiate only a 3D view and a threaded task view
+    final ViewFactory viewFactory = new ViewFactory() {
+        public View createBackgroundImageWizardStepsView(BackgroundImage backgroundImage, UserPreferences preferences,
+                                                         BackgroundImageWizardController backgroundImageWizardController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public View createFurnitureCatalogView(FurnitureCatalog catalog, UserPreferences preferences,
+                                               FurnitureCatalogController furnitureCatalogController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public View createFurnitureView(Home home, UserPreferences preferences, FurnitureController furnitureController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public HelpView createHelpView(UserPreferences preferences, HelpController helpController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public DialogView createHome3DAttributesView(UserPreferences preferences,
+                                                     Home3DAttributesController home3DAttributesController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public DialogView createHomeFurnitureView(UserPreferences preferences,
+                                                  HomeFurnitureController homeFurnitureController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public HomeView createHomeView(Home home, UserPreferences preferences, HomeController homeController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public ImportedFurnitureWizardStepsView createImportedFurnitureWizardStepsView(CatalogPieceOfFurniture piece,
+                    String modelName, boolean importHomePiece, UserPreferences preferences,
+                    ImportedFurnitureWizardController importedFurnitureWizardController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public View createImportedTextureWizardStepsView(CatalogTexture texture, String textureName,
+                                                         UserPreferences preferences,
+                                                         ImportedTextureWizardController importedTextureWizardController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public DialogView createLabelView(boolean modification, UserPreferences preferences,
+                                          LabelController labelController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public DialogView createPageSetupView(UserPreferences preferences, PageSetupController pageSetupController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public PlanView createPlanView(Home home, UserPreferences preferences, PlanController planController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public DialogView createPrintPreviewView(Home home, UserPreferences preferences, HomeController homeController,
+                                                 PrintPreviewController printPreviewController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public DialogView createRoomView(UserPreferences preferences, RoomController roomController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public TextureChoiceView createTextureChoiceView(UserPreferences preferences,
+                                                         TextureChoiceController textureChoiceController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public ThreadedTaskView createThreadedTaskView(String taskMessage, UserPreferences preferences,
+                                                       ThreadedTaskController controller) {
+          return new ThreadedTaskPanel(taskMessage, preferences, controller) {
+              private boolean taskRunning;
+  
+              public void setTaskRunning(boolean taskRunning, View executingView) {
+                if (taskRunning && !this.taskRunning) {
+                  // Display task panel directly at applet center if it's empty 
+                  this.taskRunning = taskRunning;
+                  JPanel contentPane = new JPanel(new GridBagLayout());
+                  contentPane.add(this, new GridBagConstraints());
+                  applet.setContentPane(contentPane);
+                  applet.getRootPane().revalidate();
+                } 
+              }
+            };
+        }
+
+        public DialogView createUserPreferencesView(UserPreferences preferences,
+                                                    UserPreferencesController userPreferencesController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public View createView3D(final Home home, UserPreferences preferences, final HomeController3D controller) {
+          HomeComponent3D homeComponent3D = new HomeComponent3D(home, preferences, controller);
+          // Add tab key to input map to change camera
+          InputMap inputMap = homeComponent3D.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+          inputMap.put(KeyStroke.getKeyStroke("SPACE"), "changeCamera");
+          ActionMap actionMap = homeComponent3D.getActionMap();
+          actionMap.put("changeCamera", new AbstractAction() {
+              public void actionPerformed(ActionEvent ev) {
+                if (home.getCamera() == home.getTopCamera()) {
+                  controller.viewFromObserver();
+                } else {
+                  controller.viewFromTop();
+                }
+              }
+            });
+          return homeComponent3D;
+        }
+
+        public DialogView createWallView(UserPreferences preferences, WallController wallController) {
+          throw new UnsupportedOperationException();
+        }
+
+        public DialogView createWizardView(UserPreferences preferences, WizardController wizardController) {
+          throw new UnsupportedOperationException();
+        }
+      };
+  
+    initLookAndFeel();
+
+    addComponent3DRenderingErrorObserver(applet.getRootPane(), preferences);
+
+    // Retrieve displayed home 
+    String homeUrlParameter = applet.getParameter(HOME_URL_PARAMETER);
+    if (homeUrlParameter == null) {
+      homeUrlParameter = "default.sh3d";
+    }
+    try {
+      final URL homeUrl = new URL(applet.getDocumentBase(), homeUrlParameter);
+      // Read home in a threaded task
+      Callable<Void> openTask = new Callable<Void>() {
+            public Void call() throws RecorderException {
+              // Read home with application recorder
+              Home openedHome = readHome(homeUrl);
+              displayHome(applet.getRootPane(), openedHome, viewFactory);
+              return null;
+            }
+          };
+      ThreadedTaskController.ExceptionHandler exceptionHandler = 
+          new ThreadedTaskController.ExceptionHandler() {
+            public void handleException(Exception ex) {
+              if (!(ex instanceof InterruptedRecorderException)) {
+                if (ex instanceof RecorderException) {
+                  showError(applet.getRootPane(), 
+                      preferences.getLocalizedString(ViewerHelper.class, "openError", homeUrl));
+                } else {
+                  ex.printStackTrace();
+                }
+              }
+            }
+          };
+      new ThreadedTaskController(openTask, 
+          preferences.getLocalizedString(ViewerHelper.class, "openMessage"), exceptionHandler, 
+          null, viewFactory).executeTask(null);
+    } catch (MalformedURLException ex) {
+      showError(applet.getRootPane(), 
+          preferences.getLocalizedString(ViewerHelper.class, "openError", homeUrlParameter));
+      return;
+    } 
+  }
+  
+  /**
+   * Clears all the resources used by the applet.
+   * This method is called when an applet is destroyed.  
+   */
+  public void destroy() {
+    // Collect deleted objects (seems to be required under Mac OS X when the applet is being reloaded)
+    System.gc();
+    // Stop managers threads
+    TextureManager.getInstance().clear();
+  }
+  
+  private void initLookAndFeel() {
+    try {
+      // Apply current system look and feel
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      // Enable applets to update their content while window resizing
+      Toolkit.getDefaultToolkit().setDynamicLayout(true);
+    } catch (Exception ex) {
+      // Too bad keep current look and feel
+    }
+  }
+
+  /**
+   * Sets the rendering error listener bound to Java 3D 
+   * to avoid default System exit in case of error during 3D rendering. 
+   */
+  private void addComponent3DRenderingErrorObserver(final JRootPane rootPane,
+                                                    final UserPreferences preferences) {
+    // Instead of adding a RenderingErrorListener directly to VirtualUniverse, 
+    // we add it through Canvas3DManager, because offscreen rendering needs to check 
+    // rendering errors with its own RenderingErrorListener
+    Component3DManager.getInstance().setRenderingErrorObserver(
+        new Component3DManager.RenderingErrorObserver() {
+          public void errorOccured(int errorCode, String errorMessage) {
+            System.err.print("Error in Java 3D : " + errorCode + " " + errorMessage);
+            EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                  String message = preferences.getLocalizedString(
+                      ViewerHelper.class, "3DErrorMessage");
+                  showError(rootPane, message);
+                }
+              });
+          }
+        });
+  }
+
+  /**
+   * Shows the given text in a label.
+   */
+  private static void showError(final JRootPane rootPane, String text) {
+    JLabel label = new JLabel(text, JLabel.CENTER);
+    rootPane.setContentPane(label);
+    rootPane.revalidate();
+  }
+
+  /**
+   * Reads a home from its URL.
+   */
+  private Home readHome(URL homeUrl) throws RecorderException {
+    URLConnection connection = null;
+    DefaultHomeInputStream in = null;
+    try {
+      // Open a home input stream to server 
+      connection = homeUrl.openConnection();
+      connection.setRequestProperty("Content-Type", "charset=UTF-8");
+      connection.setUseCaches(false);
+      in = new DefaultHomeInputStream(connection.getInputStream());
+      // Read home with HomeInputStream
+      Home home = in.readHome();
+      return home;
+    } catch (InterruptedIOException ex) {
+      throw new InterruptedRecorderException("Read " + homeUrl + " interrupted");
+    } catch (IOException ex) {
+      throw new RecorderException("Can't read home from " + homeUrl, ex);
+    } catch (ClassNotFoundException ex) {
+      throw new RecorderException("Missing classes to read home from " + homeUrl, ex);
+    } finally {
+      try {
+        if (in != null) {
+          in.close();
+        }
+      } catch (IOException ex) {
+        throw new RecorderException("Can't close stream", ex);
+      }
+    }
+  }
+  
+  /**
+   * Displays the given <code>home</code> in the main pane of <code>rootPane</code>. 
+   */
+  private void displayHome(final JRootPane rootPane, final Home home, 
+                           final ViewFactory viewFactory) {
+    EventQueue.invokeLater(new Runnable() {
+        public void run() {
+          HomeController3D controller = 
+              new HomeController3D(home, null, viewFactory, null, null);
+          rootPane.setContentPane((JComponent)controller.getView());
+          rootPane.revalidate();
+        }
+      });
+  }
+}
