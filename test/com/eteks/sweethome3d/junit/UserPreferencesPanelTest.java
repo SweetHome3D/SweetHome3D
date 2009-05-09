cc1d056c3cd76bdf76b2f492e08f76f41a7f67a3
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2009-05-09 08:42:14 +0000

    Tested language changes
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@1966 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- test/com/eteks/sweethome3d/junit/UserPreferencesPanelTest.java
+++ test/com/eteks/sweethome3d/junit/UserPreferencesPanelTest.java
@@ -19,30 +19,60 @@
  */
 package com.eteks.sweethome3d.junit;
 
+import java.awt.AWTException;
 import java.text.ParseException;
 import java.text.ParsePosition;
 import java.util.Locale;
+import java.util.concurrent.Callable;
 
 import javax.swing.JCheckBox;
 import javax.swing.JRadioButton;
 import javax.swing.JSpinner;
+import javax.swing.undo.UndoableEditSupport;
 
 import junit.framework.TestCase;
 
 import com.eteks.sweethome3d.io.DefaultUserPreferences;
 import com.eteks.sweethome3d.io.FileUserPreferences;
-import com.eteks.sweethome3d.model.RecorderException;
+import com.eteks.sweethome3d.model.BackgroundImage;
+import com.eteks.sweethome3d.model.Content;
+import com.eteks.sweethome3d.model.Home;
 import com.eteks.sweethome3d.model.LengthUnit;
+import com.eteks.sweethome3d.model.RecorderException;
 import com.eteks.sweethome3d.model.UserPreferences;
+import com.eteks.sweethome3d.swing.FileContentManager;
 import com.eteks.sweethome3d.swing.SwingViewFactory;
 import com.eteks.sweethome3d.swing.UserPreferencesPanel;
+import com.eteks.sweethome3d.tools.URLContent;
+import com.eteks.sweethome3d.viewcontroller.BackgroundImageWizardController;
+import com.eteks.sweethome3d.viewcontroller.FurnitureCatalogController;
+import com.eteks.sweethome3d.viewcontroller.FurnitureController;
+import com.eteks.sweethome3d.viewcontroller.HelpController;
+import com.eteks.sweethome3d.viewcontroller.Home3DAttributesController;
+import com.eteks.sweethome3d.viewcontroller.HomeController;
+import com.eteks.sweethome3d.viewcontroller.HomeController3D;
+import com.eteks.sweethome3d.viewcontroller.HomeFurnitureController;
+import com.eteks.sweethome3d.viewcontroller.ImportedFurnitureWizardController;
+import com.eteks.sweethome3d.viewcontroller.ImportedTextureWizardController;
+import com.eteks.sweethome3d.viewcontroller.LabelController;
+import com.eteks.sweethome3d.viewcontroller.PageSetupController;
+import com.eteks.sweethome3d.viewcontroller.PhotoController;
+import com.eteks.sweethome3d.viewcontroller.PlanController;
+import com.eteks.sweethome3d.viewcontroller.PrintPreviewController;
+import com.eteks.sweethome3d.viewcontroller.RoomController;
+import com.eteks.sweethome3d.viewcontroller.TextureChoiceController;
+import com.eteks.sweethome3d.viewcontroller.ThreadedTaskController;
 import com.eteks.sweethome3d.viewcontroller.UserPreferencesController;
+import com.eteks.sweethome3d.viewcontroller.WallController;
 
 /**
  * Tests {@link com.eteks.sweethome3d.swing.UserPreferencesPanel user preferences panel}.
  * @author Emmanuel Puybaret
  */
 public class UserPreferencesPanelTest extends TestCase {
+  /**
+   * Tests user preferences panel.
+   */
   public void testUserPreferencesPanel() 
        throws RecorderException, NoSuchFieldException, IllegalAccessException {
     // 0. Keep a copy of current preferences
@@ -60,6 +90,10 @@ public class UserPreferencesPanelTest extends TestCase {
         defaultPreferences.isGridVisible());
     preferences.setMagnetismEnabled(
         defaultPreferences.isMagnetismEnabled());
+    preferences.setFurnitureViewedFromTop(
+        defaultPreferences.isFurnitureViewedFromTop());
+    preferences.setFloorColoredOrTextured(
+        defaultPreferences.isFurnitureViewedFromTop());
     preferences.setNewWallThickness(
         defaultPreferences.getNewWallThickness());
     preferences.setNewWallHeight(
@@ -73,24 +107,40 @@ public class UserPreferencesPanelTest extends TestCase {
         (JRadioButton)TestUtilities.getField(panel, "centimeterRadioButton");
     JRadioButton inchRadioButton = 
         (JRadioButton)TestUtilities.getField(panel, "inchRadioButton");
+    JRadioButton meterRadioButton = 
+        (JRadioButton)TestUtilities.getField(panel, "meterRadioButton");
+    JRadioButton millimeterRadioButton = 
+        (JRadioButton)TestUtilities.getField(panel, "millimeterRadioButton");
     JCheckBox    magnetismCheckBox = 
         (JCheckBox)TestUtilities.getField(panel, "magnetismCheckBox");
     JCheckBox    rulersCheckBox = 
-      (JCheckBox)TestUtilities.getField(panel, "rulersCheckBox");
+        (JCheckBox)TestUtilities.getField(panel, "rulersCheckBox");
     JCheckBox    gridCheckBox = 
-      (JCheckBox)TestUtilities.getField(panel, "gridCheckBox");
+        (JCheckBox)TestUtilities.getField(panel, "gridCheckBox");
+    JRadioButton catalogIconRadioButton = 
+      (JRadioButton)TestUtilities.getField(panel, "catalogIconRadioButton");
+    JRadioButton topViewRadioButton = 
+        (JRadioButton)TestUtilities.getField(panel, "topViewRadioButton");
+    JRadioButton monochromeRadioButton = 
+        (JRadioButton)TestUtilities.getField(panel, "monochromeRadioButton");
+    JRadioButton floorColorOrTextureRadioButton = 
+        (JRadioButton)TestUtilities.getField(panel, "floorColorOrTextureRadioButton");
     JSpinner newWallThicknessSpinner = 
         (JSpinner)TestUtilities.getField(panel, "newWallThicknessSpinner");
     JSpinner newHomeWallHeightSpinner = 
         (JSpinner)TestUtilities.getField(panel, "newWallHeightSpinner");
     // Check panel components value
-    assertTrue("Centimeter radio button isn't selected", 
-        centimeterRadioButton.isSelected());
-    assertFalse("Inch radio button isn't selected", 
-        inchRadioButton.isSelected());
+    assertTrue("Centimeter radio button isn't selected", centimeterRadioButton.isSelected());
+    assertFalse("Inch radio button is selected",  inchRadioButton.isSelected());
+    assertFalse("Meter radio button is selected", meterRadioButton.isSelected());
+    assertFalse("Millimeter radio button is selected", millimeterRadioButton.isSelected());
     assertTrue("Magnestism isn't selected", magnetismCheckBox.isSelected());
     assertTrue("Rulers isn't selected", rulersCheckBox.isSelected());
     assertTrue("Grid isn't selected", gridCheckBox.isSelected());
+    assertTrue("Catalog icon radio button isn't selected", catalogIconRadioButton.isSelected());
+    assertFalse("Top view button is selected",  topViewRadioButton.isSelected());
+    assertTrue("Monochrome radio button isn't selected", monochromeRadioButton.isSelected());
+    assertFalse("Floor color radio button is selected",  floorColorOrTextureRadioButton.isSelected());
     assertEquals("Wrong default thickness", 
         newWallThicknessSpinner.getValue(), defaultPreferences.getNewWallThickness());
     assertEquals("Wrong default wall height", 
@@ -101,18 +151,16 @@ public class UserPreferencesPanelTest extends TestCase {
     magnetismCheckBox.setSelected(false);
     rulersCheckBox.setSelected(false);
     gridCheckBox.setSelected(false);
+    topViewRadioButton.setSelected(true);
+    floorColorOrTextureRadioButton.setSelected(true);
     newWallThicknessSpinner.setValue(1);
     newHomeWallHeightSpinner.setValue(100);
     
-    // 4. Retrieve panel values into preferences 
-    preferences.setUnit(controller.getUnit());
-    preferences.setMagnetismEnabled(controller.isMagnetismEnabled());
-    preferences.setRulersVisible(controller.isRulersVisible());
-    preferences.setGridVisible(controller.isGridVisible());
-    preferences.setNewWallThickness(controller.getNewWallThickness());
-    preferences.setNewWallHeight(controller.getNewWallHeight());
+    // 4. Retrieve panel values into preferences
+    controller.modifyUserPreferences();
     // Check preferences value
     assertPreferencesEqual(LengthUnit.INCH, false, false, false,
+        true, true,
         LengthUnit.inchToCentimeter(1), 
         LengthUnit.inchToCentimeter(100), 
         preferences);
@@ -125,6 +173,8 @@ public class UserPreferencesPanelTest extends TestCase {
         preferences.isMagnetismEnabled(), 
         preferences.isRulersVisible(), 
         preferences.isGridVisible(), 
+        preferences.isFurnitureViewedFromTop(), 
+        preferences.isRoomFloorColoredOrTextured(), 
         preferences.getNewWallThickness(),  
         preferences.getNewWallHeight(), readPreferences);
     
@@ -133,13 +183,15 @@ public class UserPreferencesPanelTest extends TestCase {
   }
   
   /**
-   * Assert values in parameter are the same as the ones 
+   * Asserts values in parameter are the same as the ones 
    * stored in <code>preferences</code>.
    */
   private void assertPreferencesEqual(LengthUnit unit,
                                       boolean magnetism,
                                       boolean rulers,
                                       boolean grid,
+                                      boolean topView,
+                                      boolean floorColorOrTexture,
                                       float newWallThickness,
                                       float newHomeWallHeight,
                                       UserPreferences preferences) {
@@ -151,6 +203,10 @@ public class UserPreferencesPanelTest extends TestCase {
         preferences.isRulersVisible());
     assertEquals("Wrong grid visibility", grid,
         preferences.isGridVisible());
+    assertEquals("Wrong furniture rendering", topView,
+        preferences.isFurnitureViewedFromTop());
+    assertEquals("Wrong room rendering", floorColorOrTexture,
+        preferences.isRoomFloorColoredOrTextured());
     assertEquals("Wrong new wall thickness", newWallThickness, 
         preferences.getNewWallThickness());
     assertEquals("Wrong new home wall height", newHomeWallHeight,
@@ -233,5 +289,60 @@ public class UserPreferencesPanelTest extends TestCase {
     ParsePosition parsePosition = new ParsePosition(0);
     LengthUnit.INCH.getFormat().parseObject("10'2A", parsePosition);
     assertEquals("Wrong parse position", "10'2A".indexOf('A'), parsePosition.getIndex());
+  }
+  
+  /**
+   * Tests language changes on the GUI. 
+   */
+  public void testLanguageChange() throws AWTException {
+    Locale.setDefault(Locale.US);
+    UserPreferences preferences = new DefaultUserPreferences() {
+      @Override
+      public void write() throws RecorderException {
+        // Ignore write requests
+      }
+    };
+
+    Home home = new Home();
+    Content imageContent = new URLContent(UserPreferencesPanelTest.class.getResource("resources/test.png"));
+    home.setBackgroundImage(new BackgroundImage(imageContent, 1, 0, 1, 0, 1, 0, 0));
+
+    SwingViewFactory viewFactory = new SwingViewFactory();
+    FileContentManager contentManager = new FileContentManager(preferences);
+    UndoableEditSupport undoableEditSupport = new UndoableEditSupport();
+    
+    for (String language : preferences.getSupportedLanguages()) {
+      preferences.setLanguage(language);
+      // Instantiate all views available in Sweet Home 3D 
+      HomeController homeController = new HomeController(home, preferences, viewFactory, contentManager);
+      homeController.getView();
+      new FurnitureCatalogController(preferences.getFurnitureCatalog(), viewFactory).getView();
+      new FurnitureController(home, preferences, viewFactory).getView();
+      new PlanController(home, preferences, viewFactory, contentManager, undoableEditSupport).getView();
+      new HomeController3D(home, preferences, viewFactory, contentManager, undoableEditSupport).getView();
+      
+      new PageSetupController(home, preferences, viewFactory, undoableEditSupport).getView();
+      new PrintPreviewController(home, preferences, homeController, viewFactory).getView();
+      new UserPreferencesController(preferences, viewFactory, contentManager).getView();
+      new HomeFurnitureController(home, preferences, viewFactory, undoableEditSupport).getView();
+      new WallController(home, preferences, viewFactory, contentManager, undoableEditSupport).getView();
+      new RoomController(home, preferences, viewFactory, contentManager, undoableEditSupport).getView();
+      new LabelController(home, preferences, viewFactory, undoableEditSupport).getView();
+      new Home3DAttributesController(home, preferences, viewFactory, contentManager, undoableEditSupport).getView();
+      new PhotoController(home, preferences, viewFactory, contentManager).getView();
+      
+      new TextureChoiceController("", preferences, viewFactory, contentManager).getView();
+      new ThreadedTaskController(new Callable<Void>() { 
+          public Void call() throws Exception {
+            return null;
+          }
+        }, "", null, preferences, viewFactory).getView();
+      
+      new BackgroundImageWizardController(home, preferences, viewFactory, contentManager, undoableEditSupport).getView();
+      new ImportedFurnitureWizardController(preferences, viewFactory, contentManager).getView();
+      new ImportedTextureWizardController(preferences, viewFactory, contentManager).getView();
+      
+      new HelpController(preferences, viewFactory).getView();
+    }
   }
 }
