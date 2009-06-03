3117507c5b5ee13978e4d582b4e446fccb68dffa
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2009-06-03 17:31:57 +0000

    Fixed a bug for automatic creation of parallel walls
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@2061 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/viewcontroller/PlanController.java
+++ src/com/eteks/sweethome3d/viewcontroller/PlanController.java
@@ -1587,23 +1587,25 @@ public class PlanController extends FurnitureController implements Controller {
    * (<code>point1</code>, <code>point2</code>) and (<code>point3</code>, <code>pont4</code>).
    */
   private float [] computeIntersection(float [] point1, float [] point2, float [] point3, float [] point4) {
-    float x = point1 [0];
-    float y = point1 [1];
+    float x = point2 [0];
+    float y = point2 [1];
     float alpha1 = (point2 [1] - point1 [1]) / (point2 [0] - point1 [0]);
     float beta1 = point2 [1] - alpha1 * point2 [0];
     float alpha2 = (point4 [1] - point3 [1]) / (point4 [0] - point3 [0]);
     float beta2 = point4 [1] - alpha2 * point4 [0];
-    // If first line is vertical
-    if (point1 [0] == point2 [0]) {
-      x = point1 [0];
-      y = alpha2 * x + beta2;
-      // If second line is vertical
-    } else if (point3 [0] == point4 [0]) {
-      x = point3 [0];
-      y = alpha1 * x + beta1;
-    } else {
-      x = (beta2 - beta1) / (alpha1 - alpha2);
-      y = alpha1 * x + beta1;
+    if (alpha1 != alpha2) {
+      // If first line is vertical
+      if (point1 [0] == point2 [0]) {
+        x = point1 [0];
+        y = alpha2 * x + beta2;
+        // If second line is vertical
+      } else if (point3 [0] == point4 [0]) {
+        x = point3 [0];
+        y = alpha1 * x + beta1;
+      } else {
+        x = (beta2 - beta1) / (alpha1 - alpha2);
+        y = alpha1 * x + beta1;
+      }
     }
     return new float [] {x, y};
   }
