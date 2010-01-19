87ca0ac5f7b34461d19424d8c34723e627858481
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2010-01-19 14:05:06 +0000

    Used same extension for temp file as at open time
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@2288 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/io/HomeFileRecorder.java
+++ src/com/eteks/sweethome3d/io/HomeFileRecorder.java
@@ -88,7 +88,7 @@ public class HomeFileRecorder implements HomeRecorder {
     File tempFile = null;
     try {
       // Open a stream on a temporary file 
-      tempFile = File.createTempFile("save", ".sh3d");
+      tempFile = File.createTempFile("save", ".sweethome3d");
       homeOut = new DefaultHomeOutputStream(new FileOutputStream(tempFile), 
           this.compressionLevel, this.includeOnlyTemporaryContent);
       // Write home with HomeOuputStream
