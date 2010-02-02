80b0454bd007dd0e35e2527760054218c95bae2a
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2010-02-02 18:16:52 +0000

    Increased max elevation
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@2320 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/swing/HomeFurniturePanel.java
+++ src/com/eteks/sweethome3d/swing/HomeFurniturePanel.java
@@ -198,7 +198,7 @@ public class HomeFurniturePanel extends JPanel implements DialogView {
     this.elevationLabel = new JLabel(SwingTools.getLocalizedLabelText(preferences, 
         HomeFurniturePanel.class, "elevationLabel.text", unitName));
     final NullableSpinner.NullableSpinnerLengthModel elevationSpinnerModel = 
-        new NullableSpinner.NullableSpinnerLengthModel(preferences, 0f, 1000f);
+        new NullableSpinner.NullableSpinnerLengthModel(preferences, 0f, 2500f);
     this.elevationSpinner = new NullableSpinner(elevationSpinnerModel);
     elevationSpinnerModel.setNullable(controller.getElevation() == null);
     elevationSpinnerModel.setLength(controller.getElevation());
