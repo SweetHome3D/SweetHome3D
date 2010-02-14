8847c7418c9049a3501a8b78b8f9098d74313b94
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2010-02-14 14:26:20 +0000

    Improved FocusTraversalPolicy
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@2379 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/swing/HomePane.java
+++ src/com/eteks/sweethome3d/swing/HomePane.java
@@ -813,16 +813,11 @@ public class HomePane extends JRootPane implements HomeView {
    * is a parent of the given child component and is too small enough to show it. 
    */
   private boolean isChildComponentInvisible(JSplitPane splitPane, Component childComponent) {
-    int dividerLocation = splitPane.getDividerLocation();
     return (SwingUtilities.isDescendingFrom(childComponent, splitPane.getTopComponent())
-           && ((dividerLocation != -1 
-                  && dividerLocation < splitPane.getMinimumDividerLocation())
-              || splitPane.getTopComponent().getWidth() == 0
+           && (splitPane.getTopComponent().getWidth() == 0
               || splitPane.getTopComponent().getHeight() == 0))
         || (SwingUtilities.isDescendingFrom(childComponent, splitPane.getBottomComponent())
-           && ((dividerLocation != -1 
-                  && dividerLocation > splitPane.getMaximumDividerLocation())
-              || splitPane.getBottomComponent().getWidth() == 0
+           && (splitPane.getBottomComponent().getWidth() == 0
               || splitPane.getBottomComponent().getHeight() == 0));
   }
 
