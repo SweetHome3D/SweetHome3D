cf1864116956257d3b43ff9a33e75217824653e9
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2010-11-25 14:32:44 +0000

    Disabled auto save if  autoSaveDelyForRecovery property isn't defined
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@2884 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/io/DefaultUserPreferences.java
+++ src/com/eteks/sweethome3d/io/DefaultUserPreferences.java
@@ -64,7 +64,12 @@ public class DefaultUserPreferences extends UserPreferences {
     setWallPattern(patternsCatalog.getPattern(getLocalizedString(DefaultUserPreferences.class, "wallPattern")));
     setNewWallThickness(Float.parseFloat(getLocalizedString(DefaultUserPreferences.class, "newWallThickness")));
     setNewWallHeight(Float.parseFloat(getLocalizedString(DefaultUserPreferences.class, "newHomeWallHeight")));
-    setAutoSaveDelayForRecovery(Integer.parseInt(getLocalizedString(DefaultUserPreferences.class, "autoSaveDelayForRecovery")));
+    try {
+      setAutoSaveDelayForRecovery(Integer.parseInt(getLocalizedString(DefaultUserPreferences.class, "autoSaveDelayForRecovery")));
+    } catch (IllegalArgumentException ex) {
+      // Disable auto save
+      setAutoSaveDelayForRecovery(0);
+    }
     setRecentHomes(new ArrayList<String>());
     try {
       setCurrency(getLocalizedString(DefaultUserPreferences.class, "currency"));
