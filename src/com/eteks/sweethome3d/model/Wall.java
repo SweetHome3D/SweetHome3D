fb1bcd82921f43b5e1237a1f1b37add50b53e226
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2011-03-06 17:45:42 +0000

    Avoid computing intersection when lines are almost parallel
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@3044 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/model/Wall.java
+++ src/com/eteks/sweethome3d/model/Wall.java
@@ -806,34 +806,43 @@ public class Wall implements Serializable, Selectable {
    */
   private void computeIntersection(float [] point1, float [] point2, 
                                    float [] point3, float [] point4, float limit) {
-    float x = point1 [0];
-    float y = point1 [1];
     float alpha1 = (point2 [1] - point1 [1]) / (point2 [0] - point1 [0]);
-    float beta1  = point2 [1] - alpha1 * point2 [0];
     float alpha2 = (point4 [1] - point3 [1]) / (point4 [0] - point3 [0]);
-    float beta2  = point4 [1] - alpha2 * point4 [0];
     // If the two lines are not parallel
     if (alpha1 != alpha2) {
+      float x = point1 [0];
+      float y = point1 [1];
+      
       // If first line is vertical
       if (Math.abs(alpha1) > 1E5)  {
         if (Math.abs(alpha2) < 1E5) {
           x = point1 [0];
+          float beta2  = point4 [1] - alpha2 * point4 [0];
           y = alpha2 * x + beta2;
         }
       // If second line is vertical
       } else if (Math.abs(alpha2) > 1E5) {
         if (Math.abs(alpha1) < 1E5) {
           x = point3 [0];
+          float beta1  = point2 [1] - alpha1 * point2 [0];
           y = alpha1 * x + beta1;
         }
-      } else  {
-        x = (beta2 - beta1) / (alpha1 - alpha2);
-        y = alpha1 * x + beta1;
-      } 
-    }
-    if (Point2D.distanceSq(x, y, point1 [0], point1 [1]) < limit * limit) {
-      point1 [0] = x;
-      point1 [1] = y;
+      } else {
+        float relativeDifference = alpha1 > alpha2 
+            ? alpha1 / alpha2
+            : alpha2 / alpha1;
+        if (relativeDifference > 1.0001) {
+          float beta1  = point2 [1] - alpha1 * point2 [0];
+          float beta2  = point4 [1] - alpha2 * point4 [0];
+          x = (beta2 - beta1) / (alpha1 - alpha2);
+          y = alpha1 * x + beta1;
+        }
+      }
+      
+      if (Point2D.distanceSq(x, y, point1 [0], point1 [1]) < limit * limit) {
+        point1 [0] = x;
+        point1 [1] = y;
+      }
     }
   }
   
--- src/com/eteks/sweethome3d/viewcontroller/PlanController.java
+++ src/com/eteks/sweethome3d/viewcontroller/PlanController.java
@@ -1752,26 +1752,33 @@ public class PlanController extends FurnitureController implements Controller {
     float x = point2 [0];
     float y = point2 [1];
     float alpha1 = (point2 [1] - point1 [1]) / (point2 [0] - point1 [0]);
-    float beta1 = point2 [1] - alpha1 * point2 [0];
     float alpha2 = (point4 [1] - point3 [1]) / (point4 [0] - point3 [0]);
-    float beta2 = point4 [1] - alpha2 * point4 [0];
 
     if (alpha1 != alpha2) {
       // If first line is vertical
       if (Math.abs(alpha1) > 1E5)  {
         if (Math.abs(alpha2) < 1E5) {
           x = point1 [0];
+          float beta2 = point4 [1] - alpha2 * point4 [0];
           y = alpha2 * x + beta2;
         }
         // If second line is vertical
       } else if (Math.abs(alpha2) > 1E5) {
         if (Math.abs(alpha1) < 1E5) {
           x = point3 [0];
+          float beta1 = point2 [1] - alpha1 * point2 [0];
           y = alpha1 * x + beta1;
         }
       } else {
-        x = (beta2 - beta1) / (alpha1 - alpha2);
-        y = alpha1 * x + beta1;
+        float relativeDifference = alpha1 > alpha2 
+            ? alpha1 / alpha2
+            : alpha2 / alpha1;
+        if (relativeDifference > 1.0001) {
+          float beta1 = point2 [1] - alpha1 * point2 [0];
+          float beta2 = point4 [1] - alpha2 * point4 [0];
+          x = (beta2 - beta1) / (alpha1 - alpha2);
+          y = alpha1 * x + beta1;
+        }
       }
     }
     return new float [] {x, y};
