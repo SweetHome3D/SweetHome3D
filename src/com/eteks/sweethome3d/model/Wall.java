be18fbab8394e4230784684f42f97767a8e56ba8
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2011-04-02 14:36:48 +0000

    Fixed intersection for walls with a negative slope
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@3078 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/model/Wall.java
+++ src/com/eteks/sweethome3d/model/Wall.java
@@ -829,13 +829,13 @@ public class Wall implements Serializable, Selectable {
         }
       } else {
         boolean sameSignum = Math.signum(alpha1) == Math.signum(alpha2);
-        if ((sameSignum && (alpha1 > alpha2   ? alpha1 / alpha2   : alpha2 / alpha1) > 1.0001)
+        if ((sameSignum && (Math.abs(alpha1) > Math.abs(alpha2)   ? alpha1 / alpha2   : alpha2 / alpha1) > 1.0001)
             || (!sameSignum && Math.abs(alpha1 - alpha2) > 1E-5)) {
           float beta1 = point2 [1] - alpha1 * point2 [0];
           float beta2 = point4 [1] - alpha2 * point4 [0];
           x = (beta2 - beta1) / (alpha1 - alpha2);
           y = alpha1 * x + beta1;
-        }
+        } 
       }
       
       if (Point2D.distanceSq(x, y, point1 [0], point1 [1]) < limit * limit) {
--- src/com/eteks/sweethome3d/viewcontroller/PlanController.java
+++ src/com/eteks/sweethome3d/viewcontroller/PlanController.java
@@ -1770,7 +1770,7 @@ public class PlanController extends FurnitureController implements Controller {
         }
       } else { 
         boolean sameSignum = Math.signum(alpha1) == Math.signum(alpha2);
-        if ((sameSignum && (alpha1 > alpha2   ? alpha1 / alpha2   : alpha2 / alpha1) > 1.0001)
+        if ((sameSignum && (Math.abs(alpha1) > Math.abs(alpha2)   ? alpha1 / alpha2   : alpha2 / alpha1) > 1.0001)
             || (!sameSignum && Math.abs(alpha1 - alpha2) > 1E-5)) {
           float beta1 = point2 [1] - alpha1 * point2 [0];
           float beta2 = point4 [1] - alpha2 * point4 [0];
