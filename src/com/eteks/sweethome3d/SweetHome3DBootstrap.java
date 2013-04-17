23ac1bc578ae37da13855010e3db1906ce81c3e9
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2013-04-17 20:34:25 +0000

    Avoided using cache to access files with jar protocol in SunFlow
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@4100 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
+++ src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
@@ -45,7 +45,7 @@ public class SweetHome3DBootstrap {
         "iText-2.1.7.jar", // Jars included in Sweet Home 3D executable jar file 
         "freehep-vectorgraphics-svg-2.1.1.jar",
         "Loader3DS1_2u.jar",
-        "sunflow-0.07.3g.jar",
+        "sunflow-0.07.3h.jar",
         "jmf.jar",
         "batik-svgpathparser-1.7.jar",
         "jnlp.jar"}));
