d63343c5eed129bc373fecbaa2866cdd8a3bd29d
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2013-07-04 17:45:14 +0000

    Fixed SVG Export under Mac OS X / Java 7
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@4207 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
+++ src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
@@ -44,7 +44,7 @@ public class SweetHome3DBootstrap {
     Class sweetHome3DBootstrapClass = SweetHome3DBootstrap.class;
     List<String> extensionJarsAndDlls = new ArrayList<String>(Arrays.asList(new String [] {
         "iText-2.1.7.jar", // Jars included in Sweet Home 3D executable jar file 
-        "freehep-vectorgraphics-svg-2.1.1.jar",
+        "freehep-vectorgraphics-svg-2.1.1a.jar",
         "Loader3DS1_2u.jar",
         "sunflow-0.07.3h.jar",
         "jmf.jar",
