4d77eea6199c81c27ef3ae8f827ef895293934fc
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2013-10-07 12:21:30 +0000

    Removed support for Java Web Start applications running with Java 6 under Mac OS X
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@4383 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
+++ src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
@@ -82,7 +82,20 @@ public class SweetHome3DBootstrap {
             + "or run Sweet Home 3D under Java 6.";
       JOptionPane.showMessageDialog(null, message);
       System.exit(1);
-    } else { // Java 6 and Java >= 1.7.0_40 
+    } else if (javaVersion.startsWith("1.6")
+               && System.getProperty("com.eteks.sweethome3d.deploymentInformation", "").startsWith("Java Web Start")) {
+      String message = Locale.getDefault().getLanguage().equals(Locale.FRENCH.getLanguage())
+          ? "Sweet Home 3D ne peut pas fonctionner avec Java\n"
+            + "Web Start %c sous Mac OS X de façon fiable.\n" 
+            + "Merci de télécharger le programme d'installation depuis\n" 
+            + "http://www.sweethome3d.com/fr/download.jsp"
+          : "Sweet Home 3D can't reliably run with Java Web Start %c\n" 
+            + "under Mac OS X.\n" 
+            + "Please download the installer version from\n" 
+            + "http://www.sweethome3d.com/download.jsp";
+      JOptionPane.showMessageDialog(null, String.format(message, System.getProperty("java.version").charAt(2)));
+      System.exit(1);
+    } else { // Java 6 and Java >= 1.7.0_40    
       extensionJarsAndDlls.addAll(Arrays.asList(new String [] {
           "macosx/java3d-1.6/j3dcore.jar", // Mac OS X Java 3D 1.6 jars and DLLs
           "macosx/java3d-1.6/vecmath.jar",
