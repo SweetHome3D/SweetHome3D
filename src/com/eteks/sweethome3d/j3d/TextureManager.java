d6acdfb6b3c5aaefd3ecd96f20103ec770f3a8b4
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2014-10-11 17:06:14 +0000

    Forced rotated texture image type to avoid losing colors
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@4931 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/j3d/TextureManager.java
+++ src/com/eteks/sweethome3d/j3d/TextureManager.java
@@ -244,7 +244,7 @@ public class TextureManager {
         double cos = Math.cos(angle);
         double sin = Math.sin(angle);
         BufferedImage rotatedImage = new BufferedImage((int)Math.round(Math.abs(image.getWidth() * cos) + Math.abs(image.getHeight() * sin)), 
-            (int)Math.round(Math.abs(image.getWidth() * sin) + Math.abs(image.getHeight() * cos)), image.getType());
+            (int)Math.round(Math.abs(image.getWidth() * sin) + Math.abs(image.getHeight() * cos)), BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2D = (Graphics2D)rotatedImage.getGraphics();
         g2D.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2D.setPaint(new TexturePaint(image, 
