0d420e8184e632770b2c1cad6ee57aa852a2436a
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2018-07-20 13:17:42 +0000

    Fixed jeksparser-calculator.jar library name
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@6408 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
+++ src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
@@ -44,7 +44,7 @@ public class SweetHome3DBootstrap {
     Class sweetHome3DBootstrapClass = SweetHome3DBootstrap.class;
     List<String> extensionJarsAndDlls = new ArrayList<String>(Arrays.asList(new String [] {
         "batik-svgpathparser-1.7.jar",
-        "jeksparsercalculator.jar",
+        "jeksparser-calculator.jar",
         "iText-2.1.7.jar", // Jars included in Sweet Home 3D executable jar file
         "freehep-vectorgraphics-svg-2.1.1b.jar",
         "sunflow-0.07.3i.jar",
