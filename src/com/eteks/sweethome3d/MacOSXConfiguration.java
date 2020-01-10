dbc1ec63af5f94fed87cc4b2bfbe03da6fd3fd11
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2020-01-10 11:34:49 +0000

    Added a copy of java.awt.desktop handlers of Java 9 to be able to compile with older Java versions
    git-svn-id: https://svn.code.sf.net/p/sweethome3d/code/trunk/SweetHome3D@6842 03e323e1-6c6c-46a5-9f71-c4ce5d8c6204

--- src/com/eteks/sweethome3d/MacOSXConfiguration.java
+++ src/com/eteks/sweethome3d/MacOSXConfiguration.java
@@ -31,21 +31,24 @@ import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Point;
 import java.awt.Window;
+import java.awt.desktop.AboutEvent;
+import java.awt.desktop.AboutHandler;
+import java.awt.desktop.OpenFilesEvent;
+import java.awt.desktop.OpenFilesHandler;
+import java.awt.desktop.PreferencesEvent;
+import java.awt.desktop.PreferencesHandler;
+import java.awt.desktop.QuitEvent;
+import java.awt.desktop.QuitHandler;
+import java.awt.desktop.QuitResponse;
+import java.awt.desktop.QuitStrategy;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowStateListener;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -71,14 +74,6 @@ import javax.swing.event.AncestorListener;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import javax.swing.event.MouseInputAdapter;
-import javax.tools.FileObject;
-import javax.tools.ForwardingJavaFileManager;
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.JavaFileObject.Kind;
-import javax.tools.SimpleJavaFileObject;
-import javax.tools.StandardJavaFileManager;
-import javax.tools.ToolProvider;
 
 import com.apple.eawt.AppEvent.FullScreenEvent;
 import com.apple.eawt.Application;
@@ -189,18 +184,48 @@ class MacOSXConfiguration {
     if (!applicationListenerSupported) {
       // Probably running under Java 9 where previous methods were removed
       if (OperatingSystem.isJavaVersionGreaterOrEqual("1.9")) {
-        MacOSXDesktopHandlersSupport.installDesktopHandlers(homeApplication, defaultController, defaultFrame);;
-
         try {
-          // Call Desktop.getDesktop().setQuitStrategy(QuitStrategy.CLOSE_ALL_WINDOWS)
-          // to prevent default call to System#exit
+          // Instantiate Java 9 handlers and set them on desktop by reflection
           Class<?> desktopClass = Class.forName("java.awt.Desktop");
           Object desktopInstance = desktopClass.getMethod("getDesktop").invoke(null);
-          Class<?> quitStrategyEnum = Class.forName("java.awt.desktop.QuitStrategy");
-          desktopClass.getMethod("setQuitStrategy", quitStrategyEnum).invoke(
-              desktopInstance, quitStrategyEnum.getField("CLOSE_ALL_WINDOWS").get(null));
-        } catch (Exception ex2) {
-          ex2.printStackTrace();
+
+          Object quitHandler = new QuitHandler() {
+              public void handleQuitRequestWith(QuitEvent ev, QuitResponse answer) {
+                MacOSXConfiguration.handleQuit(homeApplication, defaultController, defaultFrame);
+                answer.cancelQuit();
+              }
+            };
+          desktopClass.getMethod("setQuitHandler", QuitHandler.class).invoke(desktopInstance, quitHandler);
+
+          Object aboutHandler = new AboutHandler() {
+              public void handleAbout(AboutEvent ev) {
+                MacOSXConfiguration.handleAbout(homeApplication, defaultController, defaultFrame);
+              }
+            };
+          desktopClass.getMethod("setAboutHandler", AboutHandler.class).invoke(desktopInstance, aboutHandler);
+
+          Object preferencesHandler = new PreferencesHandler() {
+              public void handlePreferences(PreferencesEvent ev) {
+                MacOSXConfiguration.handlePreferences(homeApplication, defaultController, defaultFrame);
+              }
+            };
+          desktopClass.getMethod("setPreferencesHandler", PreferencesHandler.class).invoke(desktopInstance, preferencesHandler);
+
+          Object openFilesHandler = new OpenFilesHandler() {
+              public void openFiles(OpenFilesEvent ev) {
+                for (java.io.File file : ev.getFiles()) {
+                  MacOSXConfiguration.handleOpenFile(homeApplication, file.getAbsolutePath());
+                }
+              }
+            };
+          desktopClass.getMethod("setOpenFileHandler", OpenFilesHandler.class).invoke(desktopInstance, openFilesHandler);
+
+          // Call Desktop.getDesktop().setQuitStrategy(QuitStrategy.CLOSE_ALL_WINDOWS) to prevent default call to System#exit
+          desktopClass.getMethod("setQuitStrategy", QuitStrategy.class).invoke(desktopInstance, QuitStrategy.CLOSE_ALL_WINDOWS);
+        } catch (NoSuchMethodException ex) {
+        } catch (IllegalAccessException ex) {
+        } catch (InvocationTargetException ex) {
+        } catch (ClassNotFoundException ex) {
         }
       }
     }
@@ -855,170 +880,6 @@ class MacOSXConfiguration {
       // handleReOpenApplication is called when user launches
       // the application when it's already open
       MacOSXConfiguration.handleReOpenApplication(this.homeApplication);
-    }
-  }
-
-  /**
-   * Management of Mac OS X handler classes.
-   * Declared in a separated static class to be ignored with Java 5.
-   */
-  private static class MacOSXDesktopHandlersSupport {
-    static void installDesktopHandlers(SweetHome3D homeApplication,
-                                       HomeController defaultController,
-                                       JFrame defaultFrame) {
-      // Use the new handlers of Java 9 once compiling with Java 9 library is enabled
-      /*
-      java.awt.Desktop.getDesktop().setQuitHandler(new java.awt.desktop.QuitHandler() {
-          public void handleQuitRequestWith(java.awt.desktop.QuitEvent ev, java.awt.desktop.QuitResponse answer) {
-            MacOSXConfiguration.handleQuit(homeApplication, defaultController, defaultFrame);
-            answer.cancelQuit();
-          }
-        });
-      java.awt.Desktop.getDesktop().setAboutHandler(new java.awt.desktop.AboutHandler() {
-          public void handleAbout(java.awt.desktop.AboutEvent ev) {
-            MacOSXConfiguration.handleAbout(homeApplication, defaultController, defaultFrame);
-          }
-        });
-      java.awt.Desktop.getDesktop().setPreferencesHandler(new java.awt.desktop.PreferencesHandler() {
-          public void handlePreferences(java.awt.desktop.PreferencesEvent ev) {
-            MacOSXConfiguration.handlePreferences(homeApplication, defaultController, defaultFrame);
-          }
-        });
-      java.awt.Desktop.getDesktop().setOpenFileHandler(new java.awt.desktop.OpenFilesHandler() {
-          public void openFiles(java.awt.desktop.OpenFilesEvent ev) {
-            for (java.io.File file : ev.getFiles()) {
-              MacOSXConfiguration.handleOpenFile(homeApplication, file.getAbsolutePath());
-            }
-          }
-        });
-      */
-
-      try {
-        // Meanwhile compile on the fly the 4 handlers and set them by reflection
-        // Note: using compiled classes is mandatory because each handler must implement a given interface
-
-        // Create handlers source files
-        List<JavaFileObject> compilationUnits = new ArrayList<JavaFileObject>();
-        String quitHandlerClassName = "com.eteks.sweethome3d.SweetHome3DQuitHandler";
-        Class<?> quitHandlerInterface = Class.forName("java.awt.desktop.QuitHandler");
-        compilationUnits.add(createHandlerJavaFile(quitHandlerClassName,
-            quitHandlerInterface.getName(),
-            "void handleQuitRequestWith(java.awt.desktop.QuitEvent ev, java.awt.desktop.QuitResponse answer)",
-            "handleQuit",
-              "method.invoke(null, this.parameters);"
-            + "answer.cancelQuit();"));
-        String aboutHandlerClassName = "com.eteks.sweethome3d.SweetHome3DAboutHandler";
-        Class<?> aboutHandlerInterface = Class.forName("java.awt.desktop.AboutHandler");
-        compilationUnits.add(createHandlerJavaFile(aboutHandlerClassName,
-            aboutHandlerInterface.getName(),
-            "void handleAbout(java.awt.desktop.AboutEvent ev)",
-            "handleAbout", null));
-        String preferencesHandlerClassName = "com.eteks.sweethome3d.SweetHome3DPreferencesHandler";
-        Class<?> preferencesHandlerInterface = Class.forName("java.awt.desktop.PreferencesHandler");
-        compilationUnits.add(createHandlerJavaFile(preferencesHandlerClassName,
-            preferencesHandlerInterface.getName(),
-            "void handlePreferences(java.awt.desktop.PreferencesEvent ev)",
-            "handlePreferences", null));
-        String openFilesHandlerClassName = "com.eteks.sweethome3d.SweetHome3DOpenFilesHandler";
-        Class<?> openFilesHandlerInterface = Class.forName("java.awt.desktop.OpenFilesHandler");
-        compilationUnits.add(createHandlerJavaFile(openFilesHandlerClassName,
-            openFilesHandlerInterface.getName(),
-            "void openFiles(java.awt.desktop.OpenFilesEvent ev)",
-            "handleOpenFile",
-              "for (java.io.File file : ev.getFiles()) {"
-            + "  method.invoke(null, this.parameters [0], file.getAbsolutePath());"
-            + "}"));
-
-        // Compile the 4 handlers with a customized file manager which handles class files in memory
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        final Map<String, ByteArrayOutputStream> classData = new HashMap<String, ByteArrayOutputStream>();
-        ForwardingJavaFileManager<StandardJavaFileManager> fileManager =
-            new ForwardingJavaFileManager<StandardJavaFileManager>(compiler.getStandardFileManager(null, null, null)) {
-              @Override
-              public JavaFileObject getJavaFileForOutput(Location location, final String className, Kind kind, FileObject sibling) throws IOException {
-                return new SimpleJavaFileObject(URI.create("string:///" + className.replace('.', '/') + kind.extension), kind) {
-                    @Override
-                    public OutputStream openOutputStream() throws IOException {
-                      ByteArrayOutputStream data = new ByteArrayOutputStream();
-                      classData.put(className, data);
-                      return data;
-                    }
-                  };
-              }
-            };
-        // Compile with a simplified classpath to speed up compilation
-        compiler.getTask(null, fileManager, null, Arrays.asList("-cp", ""), null, compilationUnits).call();
-
-        // Retrieve the compiled handler classes
-        ClassLoader classLoader = new ClassLoader() {
-            @Override
-            protected Class<?> findClass(String className) throws ClassNotFoundException {
-              byte [] data = classData.get(className).toByteArray();
-              return super.defineClass(className, data, 0, data.length);
-            }
-          };
-        Class<?> quitHandlerClass = Class.forName(quitHandlerClassName, true, classLoader);
-        Class<?> aboutHandlerClass = Class.forName(aboutHandlerClassName, true, classLoader);
-        Class<?> preferencesHandlerClass = Class.forName(preferencesHandlerClassName, true, classLoader);
-        Class<?> openFilesHandlerClass = Class.forName(openFilesHandlerClassName, true, classLoader);
-        fileManager.close();
-
-        // Instantiate handlers and set them on desktop by reflection
-        Class<?> desktopClass = Class.forName("java.awt.Desktop");
-        Object desktopInstance = desktopClass.getMethod("getDesktop").invoke(null);
-
-        Object quitHandler = quitHandlerClass.getConstructors() [0].newInstance(MacOSXConfiguration.class, new Object [] {homeApplication, defaultController, defaultFrame});
-        desktopClass.getMethod("setQuitHandler", quitHandlerInterface).invoke(desktopInstance, quitHandler);
-
-        Object aboutHandler = aboutHandlerClass.getConstructors() [0].newInstance(MacOSXConfiguration.class, new Object [] {homeApplication, defaultController, defaultFrame});
-        desktopClass.getMethod("setAboutHandler", aboutHandlerInterface).invoke(desktopInstance, aboutHandler);
-
-        Object preferencesHandler = preferencesHandlerClass.getConstructors() [0].newInstance(MacOSXConfiguration.class, new Object [] {homeApplication, defaultController, defaultFrame});
-        desktopClass.getMethod("setPreferencesHandler", preferencesHandlerInterface).invoke(desktopInstance, preferencesHandler);
-
-        Object openFilesHandler = openFilesHandlerClass.getConstructors() [0].newInstance(MacOSXConfiguration.class, new Object [] {homeApplication});
-        desktopClass.getMethod("setOpenFileHandler", openFilesHandlerInterface).invoke(desktopInstance, openFilesHandler);
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    }
-
-    /**
-     * Returns the source of a Java class used as new Java 9 handlers.
-     */
-    private static SimpleJavaFileObject createHandlerJavaFile(final String handlerClassName,
-                                                              final String handlerInterface,
-                                                              final String handlerInterfaceMethod,
-                                                              final String configurationCalledMethod,
-                                                              final String otherCall) {
-      return new SimpleJavaFileObject(URI.create("string:///" + handlerClassName.replace('.','/') + Kind.SOURCE.extension), Kind.SOURCE) {
-          public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
-            String simpleClassName = handlerClassName.substring(handlerClassName.lastIndexOf('.') + 1);
-            return (handlerClassName.indexOf('.') > 0 ? "package " + handlerClassName.substring(0, handlerClassName.lastIndexOf('.')) + ";" : "")
-                + "public class " + simpleClassName + " implements " + handlerInterface + " {"
-                + "   private Class     configuration;"
-                + "   private Object [] parameters;"
-                + "   public " + simpleClassName + "(Class configuration, Object ... parameters) {"
-                + "      this.configuration = configuration;"
-                + "      this.parameters = parameters;"
-                + "   }"
-                + "   public " + handlerInterfaceMethod + " {"
-                        // Configuration method is searched by its name without using class types because
-                        // this class will be loaded with a separate class loader, creating a different module
-                + "     for (java.lang.reflect.Method method : this.configuration.getDeclaredMethods()) {"
-                + "       if (method.getName().equals(\""+ configurationCalledMethod + "\")) {"
-                + "         try {"
-                + "           method.setAccessible(true);"
-                + (otherCall != null ? otherCall : "method.invoke(null, this.parameters);")
-                + "         } catch (Exception ex) {"
-                + "           ex.printStackTrace();"
-                + "         }"
-                + "       }"
-                + "     }"
-                + "  }"
-                + "}";
-          }
-        };
     }
   }
 }
